9314e15b02197d55fd798133560641e11e84793b
Mike Bayer <mike_mp@zzzcomputing.com>
2005-07-07 03:36:24 +0000

    docs

---
+++ doc/build/genhtml.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import sys,re,os
+
+component_root = [
+    {'components': './components'},
+    {'content' : './content'}
+]
+doccomp = ['document_base.myt']
+output = os.path.dirname(os.getcwd())
+
+sys.path = ['./lib/'] + sys.path
+
+import documentgen
+
+documentgen.genall(doccomp, component_root, output)
+
+
+
