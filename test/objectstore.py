52d6ba4a017ba79e113b1df7f1903caebc321fdf
Mike Bayer <mike_mp@zzzcomputing.com>
2005-09-20 03:29:58 +0000

    cascading deletes !

--- test/objectstore.py
+++ test/objectstore.py
@@ -200,6 +200,9 @@ class SaveTest(AssertMixin):
             print repr(u.orders)
         #self.assert_result(l, data[0], *data[1])
         
+        print "OK " + repr(objectstore.uow().dirty.map)
+        objectstore.uow().doit = True
+        l[0].user_name = 'imnew'
         objectstore.uow().register_deleted(l[0])
         objectstore.uow().register_deleted(l[2])
         objectstore.uow().commit()
