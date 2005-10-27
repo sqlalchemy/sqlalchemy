71d9f40211ca10c62f7ace80580e4e85581ab712
Mike Bayer <mike_mp@zzzcomputing.com>
2005-10-27 06:19:28 +0000

    easyinstall build

--- setup.py
+++ setup.py
@@ -1,21 +1,26 @@
-#!/usr/bin/env python
-
-
-
-from distutils.core import setup
-import sys
-
-sys.path.append('./lib')
+from ez_setup import use_setuptools
+use_setuptools()
+from setuptools import setup, find_packages
 
 setup(name = "SQLAlchemy",
-    version = "0.91",
+    version = "0.91alpha",
     description = "Database Abstraction Library",
     author = "Mike Bayer",
     author_email = "mike_mp@zzzcomputing.com",
     url = "http://sqlalchemy.sourceforge.net",
-    packages = ["sqlalchemy", "sqlalchemy.databases"],
-    package_dir = {'' : 'lib'},
-    license = "GNU Lesser General Public License"
+    packages = find_packages('lib'),
+    package_dir = {'':'lib'},
+    license = "GNU Lesser General Public License",
+    long_description = """A Python SQL toolkit and object relational mapper for application developers.""",
+    classifiers = [
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+        "Programming Language :: Python",
+        "Topic :: Database :: Front-Ends",
+    ]
     )
+
+
 
 
