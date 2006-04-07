954fbf513da85b418e36a141645e6c8debae492d
Mike Bayer <mike_mp@zzzcomputing.com>
2006-04-07 00:25:58 +0000

    install_mod can take strings or module items

--- lib/sqlalchemy/mods/__init__.py
+++ lib/sqlalchemy/mods/__init__.py
@@ -1,4 +1,7 @@
 def install_mods(*mods):
     for mod in mods:
-        _mod = getattr(__import__('sqlalchemy.mods.%s' % mod).mods, mod)
-        _mod.install_plugin()
+        if isinstance(mod, str):
+            _mod = getattr(__import__('sqlalchemy.mods.%s' % mod).mods, mod)
+            _mod.install_plugin()
+        else:
+            mod.install_plugin()
