672a69a45b22862ad1217830752d584fcd504f4d
Mike Bayer <mike_mp@zzzcomputing.com>
2007-01-05 19:26:11 +0000

    copyright update

--- lib/sqlalchemy/util.py
+++ lib/sqlalchemy/util.py
@@ -1,5 +1,5 @@
 # util.py
-# Copyright (C) 2005,2006 Michael Bayer mike_mp@zzzcomputing.com
+# Copyright (C) 2005, 2006, 2007 Michael Bayer mike_mp@zzzcomputing.com
 #
 # This module is part of SQLAlchemy and is released under
 # the MIT License: http://www.opensource.org/licenses/mit-license.php
