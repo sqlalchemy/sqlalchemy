7838cb175913cc19ad3701a9de16ab16cdf0809f
Mike Bayer <mike_mp@zzzcomputing.com>
2007-01-20 20:40:39 +0000

    - added "validate=False" argument to assign_mapper, if True will insure that only mapped attributes are named [ticket:426]

--- lib/sqlalchemy/ext/assignmapper.py
+++ lib/sqlalchemy/ext/assignmapper.py
@@ -1,4 +1,4 @@
-from sqlalchemy import mapper, util, Query
+from sqlalchemy import mapper, util, Query, exceptions
 import types
 
 def monkeypatch_query_method(ctx, class_, name):
@@ -17,9 +17,13 @@ def monkeypatch_objectstore_method(ctx, class_, name):
     setattr(class_, name, do)
     
 def assign_mapper(ctx, class_, *args, **kwargs):
+    validate = kwargs.pop('validate', False)
     if not isinstance(getattr(class_, '__init__'), types.MethodType):
         def __init__(self, **kwargs):
              for key, value in kwargs.items():
+                 if validate:
+                     if not key in self.mapper.props:
+                         raise exceptions.ArgumentError("Invalid __init__ argument: '%s'" % key)
                  setattr(self, key, value)
         class_.__init__ = __init__
     extension = kwargs.pop('extension', None)
