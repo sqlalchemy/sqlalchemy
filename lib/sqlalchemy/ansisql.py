3e5eeb064a471269041cb12ad95f413ff9aee9c6
Mike Bayer <mike_mp@zzzcomputing.com>
2007-02-17 02:39:28 +0000

    - moved change for [ticket:466] to ansisql, since thats the syntax for all databases. the change is across all dialects, not just oracle

--- lib/sqlalchemy/ansisql.py
+++ lib/sqlalchemy/ansisql.py
@@ -746,9 +746,10 @@ class ANSISchemaGenerator(ANSISchemaBase):
     def visit_primary_key_constraint(self, constraint):
         if len(constraint) == 0:
             return
-        self.append(", \n\tPRIMARY KEY ")
+        self.append(", \n\t")
         if constraint.name is not None:
-            self.append("%s " % constraint.name)
+            self.append("CONSTRAINT %s " % constraint.name)
+        self.append("PRIMARY KEY ")
         self.append("(%s)" % (string.join([self.preparer.format_column(c) for c in constraint],', ')))
     
     def supports_alter(self):
