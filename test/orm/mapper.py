sqlalchemy/sqlalchemy

commit 8011b8f4dc2e4aff2e627bd05c1ad2afe7fe067d

- _with_parent_criterion generalized into _with_lazy_criterion - _create_lazy_clause now includes a 'reverse_direction' flag to generate lazy criterion in from parent->child or vice versa - changed join_by() in query to use the "reverse" _create_lazy_clause for instance comparisons so conditions like AND can work [ticket:554]
git-svn-id: http://svn.sqlalchemy.org/sqlalchemy/trunk@2556 8cd8332f-0806-0410-a4b6-96f4b9520244

lib/sqlalchemy/orm/strategies.py
@@ -161,7 +161,7 @@ NoLoader.logger = logging.class_logger(NoLoader)
 class LazyLoader(AbstractRelationLoader):
     def init(self):
         super(LazyLoader, self).init()
-        (self.lazywhere, self.lazybinds, self.lazyreverse) = self._create_lazy_clause(self.polymorphic_primaryjoin, self.polymorphic_secondaryjoin, self.remote_side)
+        (self.lazywhere, self.lazybinds, self.lazyreverse) = self._create_lazy_clause(self)
 
         # determine if our "lazywhere" clause is the same as the mapper's
         # get() clause.  then we can just use mapper.get()
@@ -246,12 +246,17 @@ class LazyLoader(AbstractRelationLoader):
                 # to load data into it.
                 sessionlib.attribute_manager.reset_instance_attribute(instance, self.key)
 
-    def _create_lazy_clause(self, primaryjoin, secondaryjoin, remote_side):
+    def _create_lazy_clause(cls, prop, reverse_direction=False):
+        (primaryjoin, secondaryjoin, remote_side) = (prop.polymorphic_primaryjoin, prop.polymorphic_secondaryjoin, prop.remote_side)
+        
         binds = {}
         reverse = {}
 
         def should_bind(targetcol, othercol):
-            return othercol in remote_side
+            if reverse_direction:
+                return targetcol in remote_side
+            else:
+                return othercol in remote_side
 
         def find_column_in_expr(expr):
             if not isinstance(expr, sql.ColumnElement):
@@ -300,9 +305,11 @@ class LazyLoader(AbstractRelationLoader):
             secondaryjoin = secondaryjoin.copy_container()
             lazywhere = sql.and_(lazywhere, secondaryjoin)
  
-        LazyLoader.logger.info(str(self.parent_property) + " lazy loading clause " + str(lazywhere))
+        if hasattr(cls, 'parent_property'):
+            LazyLoader.logger.info(str(cls.parent_property) + " lazy loading clause " + str(lazywhere))
         return (lazywhere, binds, reverse)
-
+    _create_lazy_clause = classmethod(_create_lazy_clause)
+    
 LazyLoader.logger = logging.class_logger(LazyLoader)
 
 

test/orm/mapper.py
@@ -387,8 +387,13 @@ class MapperTest(MapperSuperTest):
 
         # test comparing to an object instance
         item = sess.query(Item).get_by(item_name='item 4')
+
+        l = sess.query(Order).select_by(items=item)
+        self.assert_result(l, Order, user_all_result[0]['orders'][1][1])
+
         l = q.select_by(items=item)
         self.assert_result(l, User, user_result[0])
+    
     
         try:
             # this should raise AttributeError

lib/sqlalchemy/orm/query.py
@@ -365,27 +365,25 @@ class Query(object):
         t = sql.text(text)
         return self.execute(t, params=params)
 
-    def _with_parent_criterion(cls, mapper, instance, prop):
+    def _with_lazy_criterion(cls, instance, prop, reverse=False):
         """extract query criterion from a LazyLoader strategy given a Mapper, 
         source persisted/detached instance and PropertyLoader.
         
-        ."""
+        """
+        
         from sqlalchemy.orm import strategies
-        # this could be done very slickly with prop.compare() and join_via(),
-        # but we are using the less generic LazyLoader clause so that we
-        # retain the ability to do a self-referential join (also the lazy 
-        # clause typically requires only the primary table with no JOIN)
-        loader = prop._get_strategy(strategies.LazyLoader)
-        criterion = loader.lazywhere.copy_container()
-        bind_to_col = dict([(loader.lazybinds[col].key, col) for col in loader.lazybinds])
+        (criterion, lazybinds, rev) = strategies.LazyLoader._create_lazy_clause(prop, reverse_direction=reverse)
+        bind_to_col = dict([(lazybinds[col].key, col) for col in lazybinds])
 
         class Visitor(sql.ClauseVisitor):
             def visit_bindparam(self, bindparam):
+                mapper = reverse and prop.mapper or prop.parent
                 bindparam.value = mapper.get_attr_by_column(instance, bind_to_col[bindparam.key])
         Visitor().traverse(criterion)
         return criterion
-    _with_parent_criterion = classmethod(_with_parent_criterion)
+    _with_lazy_criterion = classmethod(_with_lazy_criterion)
     
+        
     def query_from_parent(cls, instance, property, **kwargs):
         """return a newly constructed Query object, with criterion corresponding to 
         a relationship to the given parent instance.
@@ -407,7 +405,7 @@ class Query(object):
         mapper = object_mapper(instance)
         prop = mapper.props[property]
         target = prop.mapper
-        criterion = cls._with_parent_criterion(mapper, instance, prop)
+        criterion = cls._with_lazy_criterion(instance, prop)
         return Query(target, **kwargs).filter(criterion)
     query_from_parent = classmethod(query_from_parent)
         
@@ -436,7 +434,7 @@ class Query(object):
                 raise exceptions.InvalidRequestError("Could not locate a property which relates instances of class '%s' to instances of class '%s'" % (self.mapper.class_.__name__, instance.__class__.__name__))
         else:
             prop = mapper.props[property]
-        return self.filter(Query._with_parent_criterion(mapper, instance, prop))
+        return self.filter(Query._with_lazy_criterion(instance, prop))
 
     def add_entity(self, entity):
         """add a mapped entity to the list of result columns to be returned.
@@ -567,7 +565,8 @@ class Query(object):
         The criterion is constructed in the same way as the
         ``select_by()`` method.
         """
-
+        import properties
+        
         clause = None
         for arg in args:
             if clause is None:
@@ -577,7 +576,10 @@ class Query(object):
 
         for key, value in params.iteritems():
             (keys, prop) = self._locate_prop(key, start=start)
-            c = prop.compare(value) & self.join_via(keys)
+            if isinstance(prop, properties.PropertyLoader):
+                c = self._with_lazy_criterion(value, prop, True) & self.join_via(keys[:-1])
+            else:
+                c = prop.compare(value) & self.join_via(keys)
             if clause is None:
                 clause =  c
             else:

commit 779068fb41a216f65f6cd18d50b261e378ed67ac

- fix to select_by(<propname>=<object instance>) -style joins in conjunction with many-to-many relationships, bug introduced in r2556 - the "reverse_direction" flag in _create_lazy_clause works correctly for a many-to-many relationship (i.e. the reverse is on which clause, not which column in the clause, in the case of m2m)

lib/sqlalchemy/orm/properties.py
@@ -418,6 +418,8 @@ class PropertyLoader(StrategizedProperty):
         if logging.is_info_enabled(self.logger):
             self.logger.info(str(self) + " setup primary join " + str(self.primaryjoin))
             self.logger.info(str(self) + " setup polymorphic primary join " + str(self.polymorphic_primaryjoin))
+            self.logger.info(str(self) + " setup secondary join " + str(self.secondaryjoin))
+            self.logger.info(str(self) + " setup polymorphic secondary join " + str(self.polymorphic_secondaryjoin))
             self.logger.info(str(self) + " foreign keys " + str([str(c) for c in self.foreign_keys]))
             self.logger.info(str(self) + " remote columns " + str([str(c) for c in self.remote_side]))
             self.logger.info(str(self) + " relation direction " + (self.direction is sync.ONETOMANY and "one-to-many" or (self.direction is sync.MANYTOONE and "many-to-one" or "many-to-many")))

lib/sqlalchemy/orm/strategies.py
@@ -253,7 +253,7 @@ class LazyLoader(AbstractRelationLoader):
         reverse = {}
 
         def should_bind(targetcol, othercol):
-            if reverse_direction:
+            if reverse_direction and not secondaryjoin:
                 return targetcol in remote_side
             else:
                 return othercol in remote_side
@@ -293,10 +293,14 @@ class LazyLoader(AbstractRelationLoader):
 
         lazywhere = primaryjoin.copy_container()
         li = mapperutil.BinaryVisitor(visit_binary)
-        li.traverse(lazywhere)
+        
+        if not secondaryjoin or not reverse_direction:
+            li.traverse(lazywhere)
         
         if secondaryjoin is not None:
             secondaryjoin = secondaryjoin.copy_container()
+            if reverse_direction:
+                li.traverse(secondaryjoin)
             lazywhere = sql.and_(lazywhere, secondaryjoin)
  
         if hasattr(cls, 'parent_property'):

test/orm/mapper.py
@@ -346,8 +346,8 @@ class MapperTest(MapperSuperTest):
         self.assert_result(k, Keyword, *item_keyword_result[1]['keywords'][1])
 
         
-    def testjoinvia(self):
-        """test the join_via and join_to functions"""
+    def testautojoin(self):
+        """test functions derived from Query's _join_to function."""
         
         m = mapper(User, users, properties={
             'orders':relation(mapper(Order, orders, properties={
@@ -386,10 +386,10 @@ class MapperTest(MapperSuperTest):
             assert False
         except AttributeError:
             assert True
-    
         
-    def testjoinviam2m(self):
-        """test the join_via and join_to functions"""
+    def testautojoinm2m(self):
+        """test functions derived from Query's _join_to function."""
+        
         m = mapper(Order, orders, properties = {
             'items' : relation(mapper(Item, orderitems, properties = {
                 'keywords' : relation(mapper(Keyword, keywords), itemkeywords)
@@ -402,10 +402,15 @@ class MapperTest(MapperSuperTest):
         l = q.filter(keywords.c.name=='square').join(['items', 'keywords']).list()
         self.assert_result(l, Order, order_result[1])
 
+        # test comparing to an object instance
+        item = sess.query(Item).selectfirst()
+        l = sess.query(Item).select_by(keywords=item.keywords[0])
+        assert item == l[0]
         
     def testcustomjoin(self):
         """test that the from_obj parameter to query.select() can be used
         to totally replace the FROM parameters of the generated query."""
+
         m = mapper(User, users, properties={
             'orders':relation(mapper(Order, orders, properties={
                 'items':relation(mapper(Item, orderitems))