721f29140623eb1eac91b1ee931ea4d907486f94
Mike Bayer <mike_mp@zzzcomputing.com>
2007-08-17 22:20:55 +0000

    add a test for dupe tables in MetaData

--- test/engine/metadata.py
+++ test/engine/metadata.py
@@ -13,6 +13,25 @@ class MetaDataTest(PersistTest):
             assert t1.count().scalar() == 0
         finally:
             metadata.drop_all()
-    
+
+
+    def test_dupe_tables(self):
+        metadata = MetaData()
+        t1 = Table('table1', metadata, Column('col1', Integer, primary_key=True),
+            Column('col2', String(20)))
+
+        metadata.bind = testbase.db
+        metadata.create_all()
+        try:
+            try:
+                t1 = Table('table1', metadata, autoload=True)
+                t2 = Table('table1', metadata, Column('col1', Integer, primary_key=True),
+                    Column('col2', String(20)))
+                assert False
+            except exceptions.ArgumentError, e:
+                assert str(e) == "Table 'table1' is already defined for this MetaData instance."
+        finally:
+            metadata.drop_all()
+            
 if __name__ == '__main__':
     testbase.main()
