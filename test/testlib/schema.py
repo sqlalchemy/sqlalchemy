1dc4546aa95d3adfff910430afe922bf137bf595
Jason Kirtland <jek@discorporate.us>
2007-12-18 19:37:03 +0000

    Apply default cascade rules for firebird self-ref ForeignKeys.

--- test/testlib/schema.py
+++ test/testlib/schema.py
@@ -1,4 +1,5 @@
 import testbase
+from testlib import testing
 schema = None
 
 __all__ = 'Table', 'Column',
@@ -11,7 +12,7 @@ def Table(*args, **kw):
     global schema
     if schema is None:
         from sqlalchemy import schema
-        
+
     test_opts = dict([(k,kw.pop(k)) for k in kw.keys()
                       if k.startswith('test_')])
 
@@ -21,6 +22,33 @@ def Table(*args, **kw):
         if 'mysql_engine' not in kw and 'mysql_type' not in kw:
             if 'test_needs_fk' in test_opts or 'test_needs_acid' in test_opts:
                 kw['mysql_engine'] = 'InnoDB'
+
+    # Apply some default cascading rules for self-referential foreign keys.
+    # MySQL InnoDB has some issues around seleting self-refs too.
+    if testing.against('firebird'):
+        table_name = args[0]
+        unpack = (testing.config.db.dialect.
+                  identifier_preparer.unformat_identifiers)
+
+        # Only going after ForeignKeys in Columns.  May need to
+        # expand to ForeignKeyConstraint too.
+        fks = [fk
+               for col in args if isinstance(col, schema.Column)
+               for fk in col.args if isinstance(fk, schema.ForeignKey)]
+
+        for fk in fks:
+            # root around in raw spec
+            ref = fk._colspec
+            if isinstance(ref, schema.Column):
+                name = ref.table.name
+            else:
+                name = unpack(ref)[-2]
+            print name, table_name
+            if name == table_name:
+                if fk.ondelete is None:
+                    fk.ondelete = 'CASCADE'
+                if fk.onupdate is None:
+                    fk.onupdate = 'CASCADE'
 
     return schema.Table(*args, **kw)
 
