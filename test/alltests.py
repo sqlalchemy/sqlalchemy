d9dce78a3bf0b2e95157e1e85b69d67af55965ab
Jason Kirtland <jek@discorporate.us>
2008-04-02 17:55:11 +0000

    - Run profiling tests first.

--- test/alltests.py
+++ test/alltests.py
@@ -10,9 +10,12 @@ import ext.alltests as ext
 import zblog.alltests as zblog
 import profiling.alltests as profiling
 
+# The profiling tests are sensitive to foibles of CPython VM state, so
+# run them first.  Ideally, each should be run in a fresh interpreter.
+
 def suite():
     alltests = unittest.TestSuite()
-    for suite in (base, engine, sql, dialect, orm, ext, zblog, profiling):
+    for suite in (profiling, base, engine, sql, dialect, orm, ext, zblog):
         alltests.addTest(suite.suite())
     return alltests
 
