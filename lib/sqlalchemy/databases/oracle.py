30ad04ba6d34fb941c110378e762c4b74d7c51b2
Jonathan Ellis <jbellis@gmail.com>
2008-05-20 23:16:45 +0000

    handle null tablespace_name

--- lib/sqlalchemy/databases/oracle.py
+++ lib/sqlalchemy/databases/oracle.py
@@ -389,10 +389,10 @@ class OracleDialect(default.DefaultDialect):
     def table_names(self, connection, schema):
         # note that table_names() isnt loading DBLINKed or synonym'ed tables
         if schema is None:
-            s = "select table_name from all_tables where tablespace_name NOT IN ('SYSTEM', 'SYSAUX')"
+            s = "select table_name from all_tables where nvl(tablespace_name, 'no tablespace') NOT IN ('SYSTEM', 'SYSAUX')"
             cursor = connection.execute(s)
         else:
-            s = "select table_name from all_tables where tablespace_name NOT IN ('SYSTEM','SYSAUX') AND OWNER = :owner"
+            s = "select table_name from all_tables where nvl(tablespace_name, 'no tablespace') NOT IN ('SYSTEM','SYSAUX') AND OWNER = :owner"
             cursor = connection.execute(s, {'owner': self._denormalize_name(schema)})
         return [self._normalize_name(row[0]) for row in cursor]
 
