4ba496442555b8808941b7674b2081452417dfd5
Mike Bayer <mike_mp@zzzcomputing.com>
2008-10-23 02:39:52 +0000

    2.4 callcounts of course go up for no apparent reason

--- test/profiling/zoomark.py
+++ test/profiling/zoomark.py
@@ -328,7 +328,7 @@ class ZooMarkTest(TestBase):
     def test_profile_1a_populate(self):
         self.test_baseline_1a_populate()
 
-    @profiling.function_call_count(322, {'2.4': 213})
+    @profiling.function_call_count(322, {'2.4': 202})
     def test_profile_2_insert(self):
         self.test_baseline_2_insert()
 
@@ -340,7 +340,7 @@ class ZooMarkTest(TestBase):
     def test_profile_4_expressions(self):
         self.test_baseline_4_expressions()
 
-    @profiling.function_call_count(1523, {'2.4': 1032})
+    @profiling.function_call_count(1523, {'2.4': 1084})
     def test_profile_5_aggregates(self):
         self.test_baseline_5_aggregates()
 
