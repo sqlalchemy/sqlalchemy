37b7e458c201a2f7788f3db8d85bed2ab2f8e190
Mike Bayer <mike_mp@zzzcomputing.com>
2009-01-14 20:48:01 +0000

    - use ForeignKey.column as _colspec source in Column._make_proxy(), preventing needless redundant string arithmetic in memoized ForeignKey.column method - _pre_existing_column attribute becomes optional, only needed for original Table-bound column, not proxies - compare two ForeignKeys based on target_fullname, don't assume self._colspec is a string - Fixed bug when overriding a Column with a ForeignKey on a reflected table, where derived columns (i.e. the "virtual" columns of a select, etc.) would inadvertently call upon schema-level cleanup logic intended only for the original column. [ticket:1278]

--- test/engine/reflection.py
+++ test/engine/reflection.py
@@ -311,6 +311,8 @@ class ReflectionTest(TestBase, ComparesTables):
                 autoload=True)
             u2 = Table('users', meta2, autoload=True)
 
+            s = sa.select([a2])
+            assert s.c.user_id
             assert len(a2.foreign_keys) == 1
             assert len(a2.c.user_id.foreign_keys) == 1
             assert len(a2.constraints) == 2
@@ -328,6 +330,8 @@ class ReflectionTest(TestBase, ComparesTables):
                 Column('user_id', sa.Integer, sa.ForeignKey('users.id')),
                 autoload=True)
 
+            s = sa.select([a2])
+            assert s.c.user_id
             assert len(a2.foreign_keys) == 1
             assert len(a2.c.user_id.foreign_keys) == 1
             assert len(a2.constraints) == 2
