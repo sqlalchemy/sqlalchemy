4928ea0e46564faf1e8a2f6c26c36fa043866ce3
Mike Bayer <mike_mp@zzzcomputing.com>
2009-03-02 00:01:09 +0000

    - updated builders for latest sphinx tip - applied patch from [ticket:1321]

--- doc/build/builder/builders.py
+++ doc/build/builder/builders.py
@@ -9,9 +9,10 @@ from pygments.lexers import PythonLexer, PythonConsoleLexer
 from pygments.formatters import HtmlFormatter, LatexFormatter
 import re
 from mako.lookup import TemplateLookup
+from mako.template import Template
 
 class MakoBridge(TemplateBridge):
-    def init(self, builder):
+    def init(self, builder, *args, **kw):
         self.layout = builder.config.html_context.get('mako_layout', 'html')
         
         self.lookup = TemplateLookup(directories=builder.config.templates_path,
@@ -28,7 +29,18 @@ class MakoBridge(TemplateBridge):
         context['mako_layout'] = self.layout == 'html' and 'static_base.mako' or 'site_base.mako'
         return self.lookup.get_template(template).render_unicode(**context)
         
-
+    
+    def render_string(self, template, context):
+        context['prevtopic'] = context.pop('prev', None)
+        context['nexttopic'] = context.pop('next', None)
+        context['mako_layout'] = self.layout == 'html' and 'static_base.mako' or 'site_base.mako'
+        return Template(template, lookup=self.lookup,
+            format_exceptions=True, 
+            imports=[
+                "from builder import util"
+            ]
+        ).render_unicode(**context)
+        
 class StripDocTestFilter(Filter):
     def filter(self, lexer, stream):
         for ttype, value in stream:
