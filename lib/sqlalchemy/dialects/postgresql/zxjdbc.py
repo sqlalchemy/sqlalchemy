90643e804775e49faf5b032cb0fcb99eb73c9e3e
Philip Jenvey <pjenvey@underboss.org>
2009-07-26 23:16:44 +0000

    remove unneeded custom ExecutionContext

--- lib/sqlalchemy/dialects/postgresql/zxjdbc.py
+++ lib/sqlalchemy/dialects/postgresql/zxjdbc.py
@@ -6,12 +6,8 @@ JDBC Driver
 The official Postgresql JDBC driver is at http://jdbc.postgresql.org/.
 
 """
-from sqlalchemy.dialects.postgresql.base import PGCompiler, PGDialect
 from sqlalchemy.connectors.zxJDBC import ZxJDBCConnector
-from sqlalchemy.engine import default
-
-class PostgreSQL_jdbcExecutionContext(default.DefaultExecutionContext):
-    pass
+from sqlalchemy.dialects.postgresql.base import PGCompiler, PGDialect
 
 class PostgreSQL_jdbcCompiler(PGCompiler):
 
@@ -19,14 +15,14 @@ class PostgreSQL_jdbcCompiler(PGCompiler):
         # Don't escape '%' like PGCompiler
         return text
 
+
 class PostgreSQL_jdbc(ZxJDBCConnector, PGDialect):
-    execution_ctx_cls = PostgreSQL_jdbcExecutionContext
     statement_compiler = PostgreSQL_jdbcCompiler
 
     jdbc_db_name = 'postgresql'
-    jdbc_driver_name = "org.postgresql.Driver"
+    jdbc_driver_name = 'org.postgresql.Driver'
 
     def _get_server_version_info(self, connection):
         return tuple(int(x) for x in connection.connection.dbversion.split('.'))
-        
+
 dialect = PostgreSQL_jdbc
