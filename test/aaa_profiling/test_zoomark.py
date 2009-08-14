e94210cdf5126633c6373a0196452f8a89ba2382
Philip Jenvey <pjenvey@underboss.org>
2009-08-14 04:01:54 +0000

    lower call counts for 2.4, hurray

--- test/aaa_profiling/test_zoomark.py
+++ test/aaa_profiling/test_zoomark.py
@@ -327,7 +327,7 @@ class ZooMarkTest(TestBase):
     def test_profile_1a_populate(self):
         self.test_baseline_1a_populate()
 
-    @profiling.function_call_count(282, {'2.4': 202})
+    @profiling.function_call_count(282, {'2.4': 186})
     def test_profile_2_insert(self):
         self.test_baseline_2_insert()
 
