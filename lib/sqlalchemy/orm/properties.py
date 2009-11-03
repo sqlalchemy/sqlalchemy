db3521823dd629d07079555341b5f5885264650f
Mike Bayer <mike_mp@zzzcomputing.com>
2009-11-03 04:58:18 +0000

    - Fixed the call to get_committed_value() on CompositeProperty. [ticket:1504]

--- lib/sqlalchemy/orm/properties.py
+++ lib/sqlalchemy/orm/properties.py
@@ -161,7 +161,8 @@ class CompositeProperty(ColumnProperty):
         return self.get_col_value(column, obj)
 
     def getcommitted(self, state, column, passive=False):
-        obj = state.get_impl(self.key).get_committed_value(state, passive=passive)
+        # TODO: no coverage here
+        obj = state.get_impl(self.key).get_committed_value(state, state.dict, passive=passive)
         return self.get_col_value(column, obj)
 
     def setattr(self, state, value, column):
