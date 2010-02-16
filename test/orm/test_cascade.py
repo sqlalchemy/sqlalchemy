sqlalchemy/sqlalchemy

commit 378ad996382539fe6cea01588d460030e4a335a2

- the "save-update" cascade will now cascade the pending *removed* values from a scalar or collection attribute into the new session during an add() operation.  This so that the flush() operation will also delete or modify rows of those disconnected items.
git-svn-id: http://svn.sqlalchemy.org/sqlalchemy/trunk@6711 8cd8332f-0806-0410-a4b6-96f4b9520244

lib/sqlalchemy/orm/properties.py
@@ -694,17 +694,24 @@ class RelationProperty(StrategizedProperty):
         else:
             passive = attributes.PASSIVE_OFF
 
-        mapper = self.mapper.primary_mapper()
-        instances = state.value_as_iterable(self.key, passive=passive)
+        if type_ == 'save-update':
+            instances = attributes.get_state_history(state, self.key, passive=passive).sum()
+        else:
+            instances = state.value_as_iterable(self.key, passive=passive)
+        
         if instances:
             for c in instances:
-                if c is not None and c not in visited_instances and (halt_on is None or not halt_on(c)):
+                if c is not None and c not in visited_instances and \
+                                        (halt_on is None or not halt_on(c)):
                     if not isinstance(c, self.mapper.class_):
                         raise AssertionError("Attribute '%s' on class '%s' doesn't handle objects "
-                                    "of type '%s'" % (self.key, str(self.parent.class_), str(c.__class__)))
+                                    "of type '%s'" % (self.key, 
+                                                        str(self.parent.class_), 
+                                                        str(c.__class__)))
                     visited_instances.add(c)
 
-                    # cascade using the mapper local to this object, so that its individual properties are located
+                    # cascade using the mapper local to this 
+                    # object, so that its individual properties are located
                     instance_mapper = object_mapper(c)
                     yield (c, instance_mapper, attributes.instance_state(c))
 

lib/sqlalchemy/orm/session.py
@@ -1049,7 +1049,8 @@ class Session(object):
         self._cascade_save_or_update(state)
 
     def _cascade_save_or_update(self, state):
-        for state, mapper in _cascade_unknown_state_iterator('save-update', state, halt_on=lambda c:c in self):
+        for state, mapper in _cascade_unknown_state_iterator(
+                                    'save-update', state, halt_on=lambda c:c in self):
             self._save_or_update_impl(state)
 
     def delete(self, instance):

test/orm/test_cascade.py
@@ -2,7 +2,7 @@
 from sqlalchemy.test.testing import assert_raises, assert_raises_message
 from sqlalchemy import Integer, String, ForeignKey, Sequence, exc as sa_exc
 from sqlalchemy.test.schema import Table, Column
-from sqlalchemy.orm import mapper, relation, create_session, class_mapper, backref
+from sqlalchemy.orm import mapper, relation, create_session, sessionmaker, class_mapper, backref
 from sqlalchemy.orm import attributes, exc as orm_exc
 from sqlalchemy.test import testing
 from sqlalchemy.test.testing import eq_
@@ -56,7 +56,26 @@ class O2MCascadeTest(_fixtures.FixtureTest):
         sess.add(o5)
         assert_raises_message(orm_exc.FlushError, "is an orphan", sess.flush)
 
-
+    @testing.resolve_artifact_names
+    def test_save_update_sends_pending(self):
+        """test that newly added and deleted collection items are cascaded on save-update"""
+        
+        sess = sessionmaker(expire_on_commit=False)()
+        o1, o2, o3 = Order(description='o1'), Order(description='o2'), Order(description='o3')
+        u = User(name='jack', orders=[o1, o2])
+        sess.add(u)
+        sess.commit()
+        sess.close()
+        
+        u.orders.append(o3)
+        u.orders.remove(o1)
+        
+        sess.add(u)
+        assert o1 in sess
+        assert o2 in sess
+        assert o3 in sess
+        sess.commit()
+        
     @testing.resolve_artifact_names
     def test_delete(self):
         sess = create_session()
@@ -400,6 +419,26 @@ class M2OCascadeTest(_base.MappedTest):
         sess.flush()
         assert prefs.count().scalar() == 2
         assert extra.count().scalar() == 2
+
+    @testing.resolve_artifact_names
+    def test_save_update_sends_pending(self):
+        """test that newly added and deleted scalar items are cascaded on save-update"""
+
+        sess = sessionmaker(expire_on_commit=False)()
+        p1, p2 = Pref(data='p1'), Pref(data='p2')
+        
+        
+        u = User(name='jack', pref=p1)
+        sess.add(u)
+        sess.commit()
+        sess.close()
+
+        u.pref = p2
+        
+        sess.add(u)
+        assert p1 in sess
+        assert p2 in sess
+        sess.commit()
 
     @testing.fails_on('maxdb', 'FIXME: unknown')
     @testing.resolve_artifact_names

lib/sqlalchemy/orm/attributes.py
@@ -1227,13 +1227,17 @@ class History(tuple):
         return self != HISTORY_BLANK
     
     def sum(self):
-        return self.added + self.unchanged + self.deleted
+        return (self.added or []) +\
+                (self.unchanged or []) +\
+                (self.deleted or [])
     
     def non_deleted(self):
-        return self.added + self.unchanged
+        return (self.added or []) +\
+                (self.unchanged or [])
     
     def non_added(self):
-        return self.unchanged + self.deleted
+        return (self.unchanged or []) +\
+                (self.deleted or [])
     
     def has_changes(self):
         return bool(self.added or self.deleted)

commit 1f7e0c069c68f60254424081bd184d2955a3b25a

- Fixed cascade bug in many-to-one relation() when attribute was set to None, introduced in r6711 (cascade deleted items into session during add()).

lib/sqlalchemy/orm/dependency.py
@@ -113,16 +113,22 @@ class DependencyProcessor(object):
         raise NotImplementedError()
 
     def _verify_canload(self, state):
-        if state is not None and not self.mapper._canload(state, allow_subtypes=not self.enable_typechecks):
+        if state is not None and \
+            not self.mapper._canload(state, allow_subtypes=not self.enable_typechecks):
             if self.mapper._canload(state, allow_subtypes=True):
-                raise exc.FlushError("Attempting to flush an item of type %s on collection '%s', "
-                                "which is not the expected type %s.  Configure mapper '%s' to load this "
-                                "subtype polymorphically, or set enable_typechecks=False to allow subtypes.  "
-                                "Mismatched typeloading may cause bi-directional relationships (backrefs) "
-                                "to not function properly." % (state.class_, self.prop, self.mapper.class_, self.mapper))
+                raise exc.FlushError(
+                    "Attempting to flush an item of type %s on collection '%s', "
+                    "which is not the expected type %s.  Configure mapper '%s' to "
+                    "load this subtype polymorphically, or set "
+                    "enable_typechecks=False to allow subtypes. "
+                    "Mismatched typeloading may cause bi-directional relationships "
+                    "(backrefs) to not function properly." % 
+                    (state.class_, self.prop, self.mapper.class_, self.mapper))
             else:
-                raise exc.FlushError("Attempting to flush an item of type %s on collection '%s', "
-                                "whose mapper does not inherit from that of %s." % (state.class_, self.prop, self.mapper.class_))
+                raise exc.FlushError(
+                    "Attempting to flush an item of type %s on collection '%s', "
+                    "whose mapper does not inherit from that of %s." % 
+                    (state.class_, self.prop, self.mapper.class_))
             
     def _synchronize(self, state, child, associationrow, clearkeys, uowcommit):
         """Called during a flush to synchronize primary key identifier

lib/sqlalchemy/orm/properties.py
@@ -704,13 +704,19 @@ class RelationProperty(StrategizedProperty):
         
         if instances:
             for c in instances:
-                if c is not None and c not in visited_instances and \
-                                        (halt_on is None or not halt_on(c)):
+                if c is not None and \
+                    c is not attributes.PASSIVE_NO_RESULT and \
+                    c not in visited_instances and \
+                    (halt_on is None or not halt_on(c)):
+                    
                     if not isinstance(c, self.mapper.class_):
-                        raise AssertionError("Attribute '%s' on class '%s' doesn't handle objects "
-                                    "of type '%s'" % (self.key, 
-                                                        str(self.parent.class_), 
-                                                        str(c.__class__)))
+                        raise AssertionError("Attribute '%s' on class '%s' "
+                                            "doesn't handle objects "
+                                            "of type '%s'" % (
+                                                self.key, 
+                                                str(self.parent.class_), 
+                                                str(c.__class__)
+                                            ))
                     visited_instances.add(c)
 
                     # cascade using the mapper local to this 

lib/sqlalchemy/orm/session.py
@@ -1051,7 +1051,7 @@ class Session(object):
 
     def _cascade_save_or_update(self, state):
         for state, mapper in _cascade_unknown_state_iterator(
-                                    'save-update', state, halt_on=lambda c:c in self):
+                                    'save-update', state, halt_on=self.__contains__):
             self._save_or_update_impl(state)
 
     def delete(self, instance):

test/orm/test_cascade.py
@@ -75,7 +75,8 @@ class O2MCascadeTest(_fixtures.FixtureTest):
         assert o2 in sess
         assert o3 in sess
         sess.commit()
-        
+
+    
     @testing.resolve_artifact_names
     def test_delete(self):
         sess = create_session()
@@ -375,8 +376,14 @@ class M2OCascadeTest(_base.MappedTest):
         Table('users', metadata,
             Column('id', Integer, primary_key=True, test_needs_autoincrement=True),
             Column('name', String(40)),
-            Column('pref_id', Integer, ForeignKey('prefs.id')))
+            Column('pref_id', Integer, ForeignKey('prefs.id')),
+            Column('foo_id', Integer, ForeignKey('foo.id'))
+            )
 
+        Table('foo', metadata,
+            Column('id', Integer, primary_key=True, test_needs_autoincrement=True),
+            Column('data', String(40))
+        )
     @classmethod
     def setup_classes(cls):
         class User(_fixtures.Base):
@@ -385,7 +392,9 @@ class M2OCascadeTest(_base.MappedTest):
             pass
         class Extra(_fixtures.Base):
             pass
-
+        class Foo(_fixtures.Base):
+            pass
+            
     @classmethod
     @testing.resolve_artifact_names
     def setup_mappers(cls):
@@ -394,8 +403,10 @@ class M2OCascadeTest(_base.MappedTest):
             extra = relation(Extra, cascade="all, delete")
         ))
         mapper(User, users, properties = dict(
-            pref = relation(Pref, lazy=False, cascade="all, delete-orphan", single_parent=True  )
+            pref = relation(Pref, lazy=False, cascade="all, delete-orphan", single_parent=True  ),
+            foo = relation(Foo) # straight m2o
         ))
+        mapper(Foo, foo)
 
     @classmethod
     @testing.resolve_artifact_names
@@ -419,6 +430,31 @@ class M2OCascadeTest(_base.MappedTest):
         sess.flush()
         assert prefs.count().scalar() == 2
         assert extra.count().scalar() == 2
+
+    @testing.resolve_artifact_names
+    def test_cascade_on_deleted(self):
+        """test a bug introduced by r6711"""
+
+        sess = sessionmaker(expire_on_commit=True)()
+        
+        
+        u1 = User(name='jack', foo=Foo(data='f1'))
+        sess.add(u1)
+        sess.commit()
+
+        u1.foo = None
+
+        # the error condition relies upon
+        # these things being true
+        assert User.foo.impl.active_history is False
+        eq_(
+            attributes.get_history(u1, 'foo'),
+            ([None], (), [attributes.PASSIVE_NO_RESULT])
+        )
+        
+        sess.add(u1)
+        assert u1 in sess
+        sess.commit()
 
     @testing.resolve_artifact_names
     def test_save_update_sends_pending(self):