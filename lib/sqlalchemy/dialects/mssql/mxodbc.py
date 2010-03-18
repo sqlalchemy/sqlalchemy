49627de3d81bd7b60b241740a4792dcf3f0bd1d5
Brad Allen <bradallen137@gmail.com>
2010-03-18 14:05:59 -0600

    Removed MxNumeric and MxFloat because now the sqlalchemy.types base classes support dialects having a class attribute of "supports_native_decimal" indicating native Python Demimal support. Adjusted mssql+mxodbc to use the sqlalchemy.types base classes for Float and Numeric instead of using the base mssql dialect's numeric types.

--- lib/sqlalchemy/dialects/mssql/mxodbc.py
+++ lib/sqlalchemy/dialects/mssql/mxodbc.py
@@ -3,10 +3,11 @@ import sys
 
 from sqlalchemy import types as sqltypes
 from sqlalchemy import util
-from sqlalchemy.connectors.mxodbc import MxODBCConnector, MxNumeric, MxFloat
+from sqlalchemy.connectors.mxodbc import MxODBCConnector
 from sqlalchemy.dialects.mssql.pyodbc import MSExecutionContext_pyodbc
-from sqlalchemy.dialects.mssql.base import MSExecutionContext, MSDialect, \
-                                            MSSQLCompiler, MSSQLStrictCompiler
+from sqlalchemy.dialects.mssql.base import (MSExecutionContext, MSDialect, 
+                                            MSSQLCompiler, MSSQLStrictCompiler,
+                                            _MSDateTime, _MSDate, TIME)
 
         
 class MSExecutionContext_mxodbc(MSExecutionContext_pyodbc):
@@ -24,17 +25,14 @@ class MSDialect_mxodbc(MxODBCConnector, MSDialect):
     # TODO: may want to use this only if FreeTDS is not in use,
     # since FreeTDS doesn't seem to use native binds.
     statement_compiler = MSSQLStrictCompiler
-
     execution_ctx_cls = MSExecutionContext_mxodbc
-    colspecs = util.update_copy(
-        MSDialect.colspecs,
-        {
-        sqltypes.Numeric : MxNumeric,
-        sqltypes.Float : MxFloat
-        },
-    )
+    colspecs = {
+        #sqltypes.Numeric : _MSNumeric,
+        sqltypes.DateTime : _MSDateTime,
+        sqltypes.Date : _MSDate,
+        sqltypes.Time : TIME,
+    }
 
-    
 
     def __init__(self, description_encoding='latin-1', **params):
         super(MSDialect_mxodbc, self).__init__(**params)
