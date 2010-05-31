1bd54fd9c854044d8153dba0f6dd261bf6bb6bff
Mike Bayer <mike_mp@zzzcomputing.com>
2010-05-31 14:47:07 -0400

    0.6.1

--- lib/sqlalchemy/__init__.py
+++ lib/sqlalchemy/__init__.py
@@ -114,6 +114,6 @@ from sqlalchemy.engine import create_engine, engine_from_config
 __all__ = sorted(name for name, obj in locals().items()
                  if not (name.startswith('_') or inspect.ismodule(obj)))
                  
-__version__ = '0.6.0'
+__version__ = '0.6.1'
 
 del inspect, sys
