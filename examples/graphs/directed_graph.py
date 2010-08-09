0daf6e174d596e14e34f098f14d298a04a1b4d9b
Mike Bayer <mike_mp@zzzcomputing.com>
2010-08-08 20:33:32 -0400

    modernized the directed graph example

--- examples/graphs/directed_graph.py
+++ examples/graphs/directed_graph.py
@@ -1,55 +1,62 @@
 """a directed graph example."""
 
-from sqlalchemy import MetaData, Table, Column, Integer, ForeignKey
-from sqlalchemy.orm import mapper, relationship, create_session
+from sqlalchemy import MetaData, Table, Column, Integer, ForeignKey, \
+    create_engine
+from sqlalchemy.orm import mapper, relationship, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 
-import logging
-logging.basicConfig()
-logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
+Base = declarative_base()
 
-meta = MetaData('sqlite://')
+class Node(Base):
+    __tablename__ = 'node'
+    
+    node_id = Column(Integer, primary_key=True)
 
-nodes = Table('nodes', meta,
-    Column("nodeid", Integer, primary_key=True)
-)
- 
-# here we have lower.nodeid <= higher.nodeid
-edges = Table('edges', meta,
-    Column("lower_id", Integer, ForeignKey('nodes.nodeid'), primary_key=True),
-    Column("higher_id", Integer, ForeignKey('nodes.nodeid'), primary_key=True)
-)
-
-meta.create_all()
-
-class Node(object):
     def __init__(self, id):
-        self.nodeid = id
-    def add_neighbor(self, othernode):
-        Edge(self, othernode)
+        self.node_id = id
+        
+    def add_neighbors(self, *nodes):
+        for node in nodes:
+            Edge(self, node)
+        return self
+        
     def higher_neighbors(self):
         return [x.higher_node for x in self.lower_edges]
+        
     def lower_neighbors(self):
         return [x.lower_node for x in self.higher_edges]
 
-class Edge(object):
+class Edge(Base):
+    __tablename__ = 'edge'
+
+    lower_id = Column(Integer, 
+                        ForeignKey('node.node_id'), 
+                        primary_key=True)
+                        
+    higher_id = Column(Integer, 
+                        ForeignKey('node.node_id'), 
+                        primary_key=True)
+
+    lower_node = relationship(Node,
+                                primaryjoin=lower_id==Node.node_id, 
+                                backref='lower_edges')
+    higher_node = relationship(Node,
+                                primaryjoin=higher_id==Node.node_id, 
+                                backref='higher_edges')
+    
+    # here we have lower.node_id <= higher.node_id
     def __init__(self, n1, n2):
-        if n1.nodeid < n2.nodeid:
+        if n1.node_id < n2.node_id:
             self.lower_node = n1
             self.higher_node = n2
         else:
             self.lower_node = n2
             self.higher_node = n1
 
-mapper(Node, nodes)
-mapper(Edge, edges, properties={
-    'lower_node':relationship(Node,
-primaryjoin=edges.c.lower_id==nodes.c.nodeid, backref='lower_edges'),
-    'higher_node':relationship(Node,
-primaryjoin=edges.c.higher_id==nodes.c.nodeid, backref='higher_edges')
-    }
-)
+engine = create_engine('sqlite://', echo=True)
+Base.metadata.create_all(engine)
 
-session = create_session()
+session = sessionmaker(engine)()
 
 # create a directed graph like this:
 #       n1 -> n2 -> n5
@@ -64,23 +71,17 @@ n5 = Node(5)
 n6 = Node(6)
 n7 = Node(7)
 
-n2.add_neighbor(n5)
-n3.add_neighbor(n6)
-n7.add_neighbor(n2)
-n1.add_neighbor(n3)
-n2.add_neighbor(n1)
+n2.add_neighbors(n5, n1)
+n3.add_neighbors(n6)
+n7.add_neighbors(n2)
+n1.add_neighbors(n3)
 
-[session.add(x) for x in [n1, n2, n3, n4, n5, n6, n7]]
-session.flush()
+session.add_all([n1, n2, n3, n4, n5, n6, n7])
+session.commit()
 
-session.expunge_all()
-
-n2 = session.query(Node).get(2)
-n3 = session.query(Node).get(3)
-
-assert [x.nodeid for x in n3.higher_neighbors()] == [6]
-assert [x.nodeid for x in n3.lower_neighbors()] == [1]
-assert [x.nodeid for x in n2.lower_neighbors()] == [1]
-assert [x.nodeid for x in n2.higher_neighbors()] == [5,7]
+assert [x.node_id for x in n3.higher_neighbors()] == [6]
+assert [x.node_id for x in n3.lower_neighbors()] == [1]
+assert [x.node_id for x in n2.lower_neighbors()] == [1]
+assert [x.node_id for x in n2.higher_neighbors()] == [5,7]
 
 
