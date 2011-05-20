ba30ce550d6f4addf9b2f2827b1c42a320b1a131
Mike Bayer <mike_mp@zzzcomputing.com>
2011-05-20 14:50:15 -0400

    dont need the DEBUG here messing up the rest of the tests

--- test/orm/test_mapper.py
+++ test/orm/test_mapper.py
@@ -1288,7 +1288,6 @@ class ORMLoggingTest(_fixtures.FixtureTest):
             logging.getLogger('sqlalchemy.orm'),
         ]:
             log.addHandler(self.buf)
-            log.setLevel(logging.DEBUG)
 
     def teardown(self):
         for log in [
