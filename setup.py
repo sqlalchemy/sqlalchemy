6d14c69edca24cd4d726949011831db74601782d
Mike Bayer <mike_mp@zzzcomputing.com>
2011-05-20 15:15:03 -0400

    this will also move back to prod/stable

--- setup.py
+++ setup.py
@@ -254,7 +254,7 @@ def run_setup(with_cext):
 
     """,
           classifiers=[
-            "Development Status :: 4 - Beta",
+            "Development Status :: 5 - Production/Stable",
             "Intended Audience :: Developers",
             "License :: OSI Approved :: MIT License",
             "Programming Language :: Python",
