6040af5d77f4cd17e63b487f088328745ad15d9e
Mike Bayer <mike_mp@zzzcomputing.com>
2011-06-22 12:02:48 -0400

    - Added ORA-00028 to disconnect codes, use     cx_oracle _Error.code to get at the code,     [ticket:2200].  Also in 0.6.9.

--- lib/sqlalchemy/dialects/oracle/cx_oracle.py
+++ lib/sqlalchemy/dialects/oracle/cx_oracle.py
@@ -686,10 +686,14 @@ class OracleDialect_cx_oracle(OracleDialect):
                     )
 
     def is_disconnect(self, e, connection, cursor):
+        error, = e.args
         if isinstance(e, self.dbapi.InterfaceError):
             return "not connected" in str(e)
         else:
-            return "ORA-03114" in str(e) or "ORA-03113" in str(e)
+            # ORA-00028: your session has been killed
+            # ORA-03114: not connected to ORACLE
+            # ORA-03113: end-of-file on communication channel
+            return error.code in (28, 3114, 3113)
 
     def create_xid(self):
         """create a two-phase transaction ID.
