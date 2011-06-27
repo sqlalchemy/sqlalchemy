0ef1ac8d43f870ee084624fc554bee8408ef934b
Mike Bayer <mike_mp@zzzcomputing.com>
2011-06-27 19:31:21 -0400

    - Fixed a subtle bug involving column     correspondence in a selectable with the     same column repeated.   Affects [ticket:2188].

--- lib/sqlalchemy/sql/expression.py
+++ lib/sqlalchemy/sql/expression.py
@@ -2116,6 +2116,11 @@ class ColumnCollection(util.OrderedProperties):
                           'another column with the same key.  Consider '
                           'use_labels for select() statements.' % (key,
                           getattr(existing, 'table', None)))
+            # pop out memoized proxy_set as this
+            # operation may very well be occurring
+            # in a _make_proxy operation
+            value.__dict__.pop('proxy_set', None)
+
         util.OrderedProperties.__setitem__(self, key, value)
 
     def remove(self, column):
@@ -2279,7 +2284,6 @@ class FromClause(Selectable):
         """
 
         # dont dig around if the column is locally present
-
         if self.c.contains_column(column):
             return column
         col, intersect = None, None
@@ -3646,6 +3650,7 @@ class ColumnClause(_Immutable, ColumnElement):
                     is_literal=is_literal
                 )
         c.proxies = [self]
+
         if attach:
             selectable.columns[c.name] = c
         return c
