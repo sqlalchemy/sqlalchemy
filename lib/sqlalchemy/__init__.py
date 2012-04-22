02e8d401fea0c77a60efcdd1138cf29b30384219
Mike Bayer <mike_mp@zzzcomputing.com>
2012-04-22 18:10:47 -0400

    0.8 initial prep

--- lib/sqlalchemy/__init__.py
+++ lib/sqlalchemy/__init__.py
@@ -117,7 +117,7 @@ from sqlalchemy.engine import create_engine, engine_from_config
 __all__ = sorted(name for name, obj in locals().items()
                  if not (name.startswith('_') or inspect.ismodule(obj)))
 
-__version__ = '0.7.7'
+__version__ = '0.8.0b1'
 
 del inspect, sys
 
