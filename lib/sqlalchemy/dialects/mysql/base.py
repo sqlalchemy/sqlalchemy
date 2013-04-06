b6d93811653f232c450bd9ce2d1d3fa90ba6cadb
Hajime Nakagami <nakagami@gmail.com>
2013-04-06 21:19:59 +0900

    select not return rowcount at mysql+cymysql

--- lib/sqlalchemy/dialects/mysql/base.py
+++ lib/sqlalchemy/dialects/mysql/base.py
@@ -2040,7 +2040,7 @@ class MySQLDialect(default.DefaultDialect):
         try:
             try:
                 rs = connection.execute(st)
-                have = rs.rowcount > 0
+                have = len(rs.fetchall()) > 0
                 rs.close()
                 return have
             except exc.DBAPIError, e:
