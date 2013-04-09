d84ae4f75468fea6b3345c1275d6a472d0cf7c5f
Mike Bayer <mike_mp@zzzcomputing.com>
2013-04-09 14:21:40 -0400

    Fixed indirect regression regarding :func:`.has_inherited_table`, where since it considers the current class' ``__table__``, was sensitive to when it was called.  This is 0.7's behavior also, but in 0.7 things tended to "work out" within events like ``__mapper_args__()``.  :func:`.has_inherited_table` now only considers superclasses, so should return the same answer regarding the current class no matter when it's called (obviously assuming the state of the superclass). [ticket:2656]

--- lib/sqlalchemy/ext/declarative/api.py
+++ lib/sqlalchemy/ext/declarative/api.py
@@ -38,7 +38,7 @@ def has_inherited_table(cls):
     """Given a class, return True if any of the classes it inherits from has a
     mapped table, otherwise return False.
     """
-    for class_ in cls.__mro__:
+    for class_ in cls.__mro__[1:]:
         if getattr(class_, '__table__', None) is not None:
             return True
     return False
