2cf49d898d3a10d1c7a5ac87dd015691d51f9248
Mike Bayer <mike_mp@zzzcomputing.com>
2013-04-27 17:27:13 -0400

    0.8.1

--- doc/build/conf.py
+++ doc/build/conf.py
@@ -83,9 +83,9 @@ copyright = u'2007-2013, the SQLAlchemy authors and contributors'
 # The short X.Y version.
 version = "0.8"
 # The full version, including alpha/beta/rc tags.
-release = "0.8.0"
+release = "0.8.1"
 
-release_date = "March 9, 2013"
+release_date = "April 27, 2013"
 
 site_base = "http://www.sqlalchemy.org"
 
