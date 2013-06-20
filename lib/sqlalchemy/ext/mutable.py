a2a570e9f48e1f6adf5f41fef1d837a37bbf905d
Devi <asldevi@gmail.com>
2013-06-20 16:42:19 +0530

    make tests pass

--- lib/sqlalchemy/ext/mutable.py
+++ lib/sqlalchemy/ext/mutable.py
@@ -591,7 +591,7 @@ def _setup_composite_listener():
     import types
     def _listen_for_type(mapper, class_):
         for prop in mapper.iterate_properties:
-            if (hasattr(prop, 'composite_class') and (type(prop.composite_class) is types.ClassType) and
+            if (hasattr(prop, 'composite_class') and (type(prop.composite_class) in (types.ClassType, types.TypeType)) and
                 issubclass(prop.composite_class, MutableComposite)):
                 prop.composite_class._listen_on_attribute(
                     getattr(class_, prop.key), False, class_)
