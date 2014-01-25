9ad0e9a842be2e365d81c2fc23931108fa691094
Mike Bayer <mike_mp@zzzcomputing.com>
2014-01-25 17:26:07 -0500

    hypothetical "floaty thing" for navigation, doesn't really work yet

--- doc/build/static/init.js
+++ doc/build/static/init.js
@@ -4,10 +4,17 @@ function initSQLPopups() {
     $('a.sql_link').click(function() {
         $(this).nextAll('div.popup_sql:first').toggle();
         return false;
-    })
+    });
 }
+
+/*function initFloatyThings() {
+	$("dl.function, dl.class, dl.method, dl.attr, dl.data").each(function(idx, elem) {
+		$(elem).prepend("<div class='floatything'>" + $(elem).contents("dt").attr('id')+ "</div>");
+	});
+}*/
 
 $(document).ready(function() {
     initSQLPopups();
+    /*initFloatyThings();*/
 });
 
