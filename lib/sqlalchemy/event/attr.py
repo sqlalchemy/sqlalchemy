8878e2307954a98c894441613b6e836b3982556f
Mike Bayer <mike_mp@zzzcomputing.com>
2014-02-19 16:11:19 -0500

    restore the check ahead of the lock to avoid using it after initialization is done

--- lib/sqlalchemy/event/attr.py
+++ lib/sqlalchemy/event/attr.py
@@ -240,12 +240,13 @@ class _CompoundListener(_HasParentDispatchDescriptor):
         """Execute this event, but only if it has not been
         executed already for this collection."""
 
-        with self._exec_once_mutex:
-            if not self._exec_once:
-                try:
-                    self(*args, **kw)
-                finally:
-                    self._exec_once = True
+        if not self._exec_once:
+            with self._exec_once_mutex:
+                if not self._exec_once:
+                    try:
+                        self(*args, **kw)
+                    finally:
+                        self._exec_once = True
 
     def __call__(self, *args, **kw):
         """Execute this event."""
