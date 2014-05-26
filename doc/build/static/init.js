2ce24b64e17075726761eb7b017642ea05c72479
Mike Bayer <mike_mp@zzzcomputing.com>
2014-05-26 16:40:24 -0400

    more tweaks

--- doc/build/static/init.js
+++ doc/build/static/init.js
@@ -11,30 +11,48 @@ var automatedBreakpoint = -1;
 
 function initFloatyThings() {
 
-    automatedBreakpoint = $("#docs-top-navigation-container").offset().top;
-    autoOffset = $("#docs-container").offset().top;
+    automatedBreakpoint = $("#docs-top-navigation-container").position().top;
+
+    parentOffset = $("#docs-container").parent().position().top - $("#docs-container").position().top;
+    containerHeight = $("#docs-top-navigation-container").height();
+
+    sidebar = $("#docs-sidebar").position();
+    if (sidebar) {
+        margin = sidebar.top - (automatedBreakpoint + containerHeight);
+    }
+    else {
+        margin = 0;
+    }
+    autoOffset = containerHeight + margin - parentOffset;
+
+    $("#docs-sidebar").addClass("preautomated");
+    $("#docs-container").addClass("preautomated");
+
+
     function setNavSize() {
         $("#docs-top-navigation-container").css("width", $("#docs-container").width());
     }
 
     function setScroll() {
+
         var scrolltop = $(window).scrollTop();
         if (scrolltop >= automatedBreakpoint) {
             setNavSize();
             $("#docs-top-navigation-container").addClass("automated");
             $("#docs-sidebar").addClass("automated");
-            $("#docs-sidebar").css("top", $("#docs-top-navigation-container").height());
-            $("#docs-body").css("margin-top",
-                                    $("#docs-top-navigation-container").height() +
-                                    autoOffset);
+            $("#docs-container").addClass("automated");
+            $("#docs-body").css("margin-top", autoOffset);
         }
         else {
             $("#docs-top-navigation-container.automated").css("width", "");
             $("#docs-sidebar.automated").scrollTop(0);
             $("#docs-top-navigation-container").removeClass("automated");
+            $("#docs-container").removeClass("automated");
             $("#docs-sidebar").removeClass("automated");
             $("#docs-body").css("margin-top", "");
         }
+
+
     }
     $(window).scroll(setScroll)
 
