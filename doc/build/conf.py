807680a9ec1e866d7eee997ca051c1b14964d742
Mike Bayer <mike_mp@zzzcomputing.com>
2014-07-22 16:56:07 -0400

    0.8.7

--- doc/build/conf.py
+++ doc/build/conf.py
@@ -84,9 +84,9 @@ copyright = u'2007-2014, the SQLAlchemy authors and contributors'
 # The short X.Y version.
 version = "0.8"
 # The full version, including alpha/beta/rc tags.
-release = "0.8.6"
+release = "0.8.7"
 
-release_date = "March 28, 2014"
+release_date = "July 22, 2014"
 
 site_base = os.environ.get("RTD_SITE_BASE", "http://www.sqlalchemy.org")
 
