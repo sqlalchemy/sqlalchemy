54d086253e9bf31fa4d3764e9ff3a593b3c9c28e
Mike Bayer <mike_mp@zzzcomputing.com>
2014-07-25 18:51:44 -0400

    - restore non_updating_cascade to test_manytomany_nonpassive, but also add sane_multi_rowcount requirement, as pg8000 doesn't do "multi" row count.

--- test/orm/test_naturalpks.py
+++ test/orm/test_naturalpks.py
@@ -393,6 +393,8 @@ class NaturalPKTest(fixtures.MappedTest):
     def test_manytomany_passive(self):
         self._test_manytomany(True)
 
+    @testing.requires.non_updating_cascade
+    @testing.requires.sane_multi_rowcount
     def test_manytomany_nonpassive(self):
         self._test_manytomany(False)
 
