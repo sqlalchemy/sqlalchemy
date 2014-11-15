5fd779df0c958dc1ec2766f55d80b3090d2427eb
Jon Nelson <jnelson@jamponi.net>
2014-11-14 22:19:26 -0600

    - fix minor spelling error

--- test/requirements.py
+++ test/requirements.py
@@ -30,7 +30,7 @@ def exclude(db, op, spec, description=None):
 class DefaultRequirements(SuiteRequirements):
     @property
     def deferrable_or_no_constraints(self):
-        """Target database must support derferable constraints."""
+        """Target database must support deferrable constraints."""
 
         return skip_if([
             no_support('firebird', 'not supported by database'),
