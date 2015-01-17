96ef0bf768c8073d999888c3afba248442db5b93
Mike Bayer <mike_mp@zzzcomputing.com>
2015-01-17 12:46:20 -0500

    - add an exclusion here that helps with the case of 3rd party test suite redefining an existing test in test_suite
    Conflicts:
    	lib/sqlalchemy/testing/plugin/pytestplugin.py

--- lib/sqlalchemy/testing/plugin/pytestplugin.py
+++ lib/sqlalchemy/testing/plugin/pytestplugin.py
@@ -44,6 +44,10 @@ def pytest_collection_modifyitems(session, config, items):
     # new classes to a module on the fly.
 
     rebuilt_items = collections.defaultdict(list)
+    items[:] = [
+        item for item in
+        items if isinstance(item.parent, pytest.Instance)
+        and not item.parent.parent.name.startswith("_")]
     test_classes = set(item.parent for item in items)
     for test_class in test_classes:
         for sub_cls in plugin_base.generate_sub_tests(
