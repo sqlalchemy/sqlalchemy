dc22f66d943aca78e9188ba501d8c6ae009d6c63
Mike Bayer <mike_mp@zzzcomputing.com>
2016-01-20 15:58:24 -0500

    - update oracle JDBC driver URL, fixes #3554
    (cherry picked from commit 24dba714cb56e1f5a4dca5453cc5f442e6ee08ab)

--- lib/sqlalchemy/dialects/oracle/zxjdbc.py
+++ lib/sqlalchemy/dialects/oracle/zxjdbc.py
@@ -10,8 +10,7 @@
     :name: zxJDBC for Jython
     :dbapi: zxjdbc
     :connectstring: oracle+zxjdbc://user:pass@host/dbname
-    :driverurl: http://www.oracle.com/technology/software/tech/java/\
-sqlj_jdbc/index.html.
+    :driverurl: http://www.oracle.com/technetwork/database/features/jdbc/index-091264.html
 
     .. note:: Jython is not supported by current versions of SQLAlchemy.  The
        zxjdbc dialect should be considered as experimental.
