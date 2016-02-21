a1c1ce7f769fe0e6cef42fe2aec1bb8033620cbc
Mike Bayer <mike_mp@zzzcomputing.com>
2016-02-20 20:30:49 -0500

    - version bump

--- lib/sqlalchemy/__init__.py
+++ lib/sqlalchemy/__init__.py
@@ -120,7 +120,7 @@ from .schema import (
 from .inspection import inspect
 from .engine import create_engine, engine_from_config
 
-__version__ = '1.0.12'
+__version__ = '1.0.13'
 
 
 def __go(lcls):
