45a268b1b09669de80e1104bc2730322beda2db6
Mike Bayer <mike_mp@zzzcomputing.com>
2017-01-09 14:17:37 -0500

    - prep for 1.0.17
    Change-Id: I686bcadd54ea674c011bb3777e410b69627bab72

--- lib/sqlalchemy/__init__.py
+++ lib/sqlalchemy/__init__.py
@@ -121,7 +121,7 @@ from .schema import (
 from .inspection import inspect
 from .engine import create_engine, engine_from_config
 
-__version__ = '1.0.16'
+__version__ = '1.0.17'
 
 
 def __go(lcls):
