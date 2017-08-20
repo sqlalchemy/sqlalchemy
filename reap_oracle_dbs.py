cc741e1ac7fce9ce7117b4855d56e38749c04868
Mike Bayer <mike_mp@zzzcomputing.com>
2017-08-20 18:21:34 -0400

    - rework oracle de-provisioning to write URLs to the file as well, supporting custom dburi etc.
    Change-Id: Ic0ab0b3b4223e40fd335ee3313fda4dfce942100

--- reap_oracle_dbs.py
+++ reap_oracle_dbs.py
@@ -6,8 +6,6 @@ TCP connection even if close() is called, which prevents the provisioning
 system from dropping a database in-process.
 
 """
-from sqlalchemy.testing.plugin import plugin_base
-from sqlalchemy.testing import engines
 from sqlalchemy.testing import provision
 import logging
 import sys
@@ -15,11 +13,6 @@ import sys
 logging.basicConfig()
 logging.getLogger(provision.__name__).setLevel(logging.INFO)
 
-plugin_base.read_config()
-oracle = plugin_base.file_config.get('db', 'oracle')
-from sqlalchemy.testing import provision
-
-engine = engines.testing_engine(oracle, {})
-provision.reap_oracle_dbs(engine, sys.argv[1])
+provision.reap_oracle_dbs(sys.argv[1])
 
 
