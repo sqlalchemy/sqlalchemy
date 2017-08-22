df01130bb12f789e233c79363912322463ca1591
Mike Bayer <mike_mp@zzzcomputing.com>
2017-08-22 13:07:24 -0400

    - repair --dbs
    Change-Id: I69e39d2368f50b126c369ecc35e01799fd013254
    (cherry picked from commit 3fc6f32ddc5fbbf439acff42c2fdae9e910154be)

--- lib/sqlalchemy/testing/plugin/pytestplugin.py
+++ lib/sqlalchemy/testing/plugin/pytestplugin.py
@@ -30,6 +30,26 @@ def pytest_addoption(parser):
                     callback_(option_string, values, parser)
             kw["action"] = CallableAction
 
+        zeroarg_callback = kw.pop("zeroarg_callback", None)
+        if zeroarg_callback:
+            class CallableAction(argparse.Action):
+                def __init__(self, option_strings,
+                             dest, default=False,
+                             required=False, help=None):
+                        super(CallableAction, self).__init__(
+                            option_strings=option_strings,
+                            dest=dest,
+                            nargs=0,
+                            const=True,
+                            default=default,
+                            required=required,
+                            help=help)
+
+                def __call__(self, parser, namespace,
+                             values, option_string=None):
+                    zeroarg_callback(option_string, values, parser)
+            kw["action"] = CallableAction
+
         group.addoption(name, **kw)
 
     plugin_base.setup_options(make_option)
