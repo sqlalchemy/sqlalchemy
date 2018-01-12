0b04471331238cf505bcf7dee03b2fd5637be251
Mike Bayer <mike_mp@zzzcomputing.com>
2018-01-12 14:23:53 -0500

    happy new year
    Change-Id: I3ef36bfd0cb0ba62b3123c8cf92370a43156cf8f
    (cherry picked from commit 699272e4dcb9aa71ebbc0d9487fb6de82d3abc2b)

--- lib/sqlalchemy/log.py
+++ lib/sqlalchemy/log.py
@@ -1,5 +1,5 @@
 # sqlalchemy/log.py
-# Copyright (C) 2006-2017 the SQLAlchemy authors and contributors
+# Copyright (C) 2006-2018 the SQLAlchemy authors and contributors
 # <see AUTHORS file>
 # Includes alterations by Vinay Sajip vinay_sajip@yahoo.co.uk
 #
