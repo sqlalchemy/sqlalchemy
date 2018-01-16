42cf8ea91d2604a3b705e9f8eccbaf5e212ca572
Mike Bayer <mike_mp@zzzcomputing.com>
2018-01-16 13:02:17 -0500

    Add 10054 to ODBC / SQL Server disconnect codes
    Change-Id: I6e2518232608f47515b96984b4b7a41b8385975a
    Fixes: #4164

--- lib/sqlalchemy/dialects/mssql/pyodbc.py
+++ lib/sqlalchemy/dialects/mssql/pyodbc.py
@@ -299,7 +299,8 @@ class MSDialect_pyodbc(PyODBCConnector, MSDialect):
         if isinstance(e, self.dbapi.Error):
             for code in (
                     '08S01', '01002', '08003', '08007',
-                    '08S02', '08001', 'HYT00', 'HY010'):
+                    '08S02', '08001', 'HYT00', 'HY010',
+                    '10054'):
                 if code in str(e):
                     return True
         return super(MSDialect_pyodbc, self).is_disconnect(
