92310c1fd45fc492a853a723dd09e7f00d2f582f
Mike Bayer <mike_mp@zzzcomputing.com>
2018-04-11 09:29:00 -0400

    Correct join for FKs with schema in SQL Server
    Fixed 1.2 regression caused by :ticket:`4060` where the query used to
    reflect SQL Server cross-schema foreign keys was limiting the criteria
    incorrectly.
    
    Additionally, added some rework of the inter-schema reflection tests
    so that MySQL, MSSQL can be included, breaking out some of the
    Postgresql-specific behaviors into separate requirements.
    
    Fixes: #4234
    Change-Id: I20c8e70707075f1767b79127c2c27d4b313c6515
    (cherry picked from commit 9d5e117f6fcc38d8773bc943c615888dc8a3a819)

--- lib/sqlalchemy/dialects/mssql/base.py
+++ lib/sqlalchemy/dialects/mssql/base.py
@@ -2241,7 +2241,7 @@ class MSDialect(default.DefaultDialect):
                         RR.c.delete_rule],
                        sql.and_(C.c.table_name == tablename,
                                 C.c.table_schema == owner,
-                                R.c.table_schema == C.c.table_schema,
+                                RR.c.constraint_schema == C.c.table_schema,
                                 C.c.constraint_name == RR.c.constraint_name,
                                 R.c.constraint_name ==
                                 RR.c.unique_constraint_name,
