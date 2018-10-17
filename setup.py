b4d42a84e22652c16b914a2f76421718be913027
Mike Bayer <mike_mp@zzzcomputing.com>
2018-10-17 11:49:47 -0400

    block py.test 3.9.1
    references:
    
    https://github.com/pytest-dev/pytest/issues/4181
    
    Change-Id: I216fde19dd0f5c4910d76c08400f2329b09ad584

--- setup.py
+++ setup.py
@@ -139,7 +139,7 @@ def run_setup(with_cext):
         package_dir={'': 'lib'},
         license="MIT License",
         cmdclass=cmdclass,
-        tests_require=['pytest >= 2.5.2', 'mock', 'pytest-xdist'],
+        tests_require=['pytest>=2.5.2,!=3.9.1', 'mock', 'pytest-xdist'],
         long_description=readme,
         python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
         classifiers=[
@@ -164,7 +164,7 @@ def run_setup(with_cext):
             'mysql': ['mysqlclient'],
             'pymysql': ['pymysql'],
             'postgresql': ['psycopg2'],
-            'postgresql_psycopg2binary': ['psycopg2-binary'],            
+            'postgresql_psycopg2binary': ['psycopg2-binary'],
             'postgresql_pg8000': ['pg8000'],
             'postgresql_psycopg2cffi': ['psycopg2cffi'],
             'oracle': ['cx_oracle'],
