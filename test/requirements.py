08994cb97c501a3cf984fd827eba9aa9614b9dd3
Mike Bayer <mike_mp@zzzcomputing.com>
2019-01-09 11:42:02 -0500

    Skip expression-based index reflection for SQLite
    Reflection of an index based on SQL expressions are now skipped with a
    warning, in the same way as that of the Postgresql dialect, where we currently
    do not support reflecting indexes that have SQL expressions within them.
    Previously, an index with columns of None were produced which would break
    tools like Alembic.
    
    Fixes: #4431
    Change-Id: I1363ade912d206b42669331e2be2bb6f444b65a2

--- test/requirements.py
+++ test/requirements.py
@@ -457,6 +457,10 @@ class DefaultRequirements(SuiteRequirements):
         )
 
     @property
+    def indexes_with_expressions(self):
+        return only_on(["postgresql", "sqlite>=3.9.0"])
+
+    @property
     def temp_table_names(self):
         """target dialect supports listing of temporary table names"""
 
