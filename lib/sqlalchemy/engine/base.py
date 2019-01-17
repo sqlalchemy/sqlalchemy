448665f208b6194fcd45fdfe5c8f86a78d69cb79
Mike Bayer <mike_mp@zzzcomputing.com>
2019-01-16 20:20:45 -0500

    - pep8 fix
    Change-Id: I5e2e013c3460c035ad80dae7fd08efe5d1676a5d
    (cherry picked from commit 313be7c78db070169bd863948f922c74871d7000)

--- lib/sqlalchemy/engine/base.py
+++ lib/sqlalchemy/engine/base.py
@@ -590,7 +590,8 @@ class Connection(Connectable):
 
             :meth:`.Connection.begin_nested` - use a SAVEPOINT
 
-            :meth:`.Connection.begin_twophase` - use a two phase /XID transaction
+            :meth:`.Connection.begin_twophase` -
+            use a two phase /XID transaction
 
             :meth:`.Engine.begin` - context manager available from
             :class:`.Engine`
