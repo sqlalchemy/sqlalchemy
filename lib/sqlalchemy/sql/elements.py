e03f7ab50e69ffce67585cdc7a0cb7a3cc5c0cc1
Mike Bayer <mike_mp@zzzcomputing.com>
2019-07-04 11:16:50 -0400

    Add tutorial section for cast(), type_coerce()
    Change-Id: I49f635f0ad4d07abe8ef2681c9660ec7fcf5f99b

--- lib/sqlalchemy/sql/elements.py
+++ lib/sqlalchemy/sql/elements.py
@@ -859,6 +859,14 @@ class ColumnElement(
 
         This is a shortcut to the :func:`~.expression.cast` function.
 
+        .. seealso::
+
+            :ref:`coretutorial_casts`
+
+            :func:`~.expression.cast`
+
+            :func:`~.expression.type_coerce`
+
         .. versionadded:: 1.0.7
 
         """
@@ -2458,7 +2466,13 @@ class Cast(ColumnElement):
 
     .. seealso::
 
+        :ref:`coretutorial_casts`
+
         :func:`.cast`
+
+        :func:`.type_coerce` - an alternative to CAST that coerces the type
+        on the Python side only, which is often sufficient to generate the
+        correct SQL and data coercion.
 
     """
 
@@ -2509,8 +2523,12 @@ class Cast(ColumnElement):
 
         .. seealso::
 
-            :func:`.type_coerce` - Python-side type coercion without emitting
-            CAST.
+            :ref:`coretutorial_casts`
+
+            :func:`.type_coerce` - an alternative to CAST that coerces the type
+            on the Python side only, which is often sufficient to generate the
+            correct SQL and data coercion.
+
 
         """
         self.type = type_api.to_instance(type_)
@@ -2551,6 +2569,8 @@ class TypeCoerce(ColumnElement):
     .. seealso::
 
         :func:`.expression.type_coerce`
+
+        :func:`.cast`
 
     """
 
@@ -2613,6 +2633,8 @@ class TypeCoerce(ColumnElement):
          the type to which the expression is coerced.
 
         .. seealso::
+
+            :ref:`coretutorial_casts`
 
             :func:`.cast`
 
