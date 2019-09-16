d3eecfb7013a70127e39e73e9ddfde8a18689c7f
Stepland <16676308+Stepland@users.noreply.github.com>
2019-09-16 11:40:29 -0400

    Add note on the use of tuple_() for the IN operator in the docs
    Closes: #4861
    Pull-request: https://github.com/sqlalchemy/sqlalchemy/pull/4861
    Pull-request-sha: c7379d390752d0c10d6488872b163b06ee30d952
    
    Change-Id: I223008f720fe64951e2a0bf95aab955ece22516b
    (cherry picked from commit 6a60d7305bafc311115543e0c2cb5136a254edf2)

--- lib/sqlalchemy/sql/elements.py
+++ lib/sqlalchemy/sql/elements.py
@@ -2108,7 +2108,8 @@ class Tuple(ClauseList, ColumnElement):
     def __init__(self, *clauses, **kw):
         """Return a :class:`.Tuple`.
 
-        Main usage is to produce a composite IN construct::
+        Main usage is to produce a composite IN construct using
+        :meth:`.ColumnOperators.in_` ::
 
             from sqlalchemy import tuple_
 
