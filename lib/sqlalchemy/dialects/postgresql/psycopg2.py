383be8ee6f1b9eefe59f222b31e88f654b8c0f56
Kevin Kirsche <kevin.kirsche@one.verizon.com>
2021-09-10 14:15:18 -0400

    fix: lib/sqlalchemy/dialects/postgresql/psycopg2.py

--- lib/sqlalchemy/dialects/postgresql/psycopg2.py
+++ lib/sqlalchemy/dialects/postgresql/psycopg2.py
@@ -1037,7 +1037,7 @@ class PGDialect_psycopg2(PGDialect):
                 "connection not open",
                 "could not receive data from server",
                 "could not send data to server",
-                # psycopg2 client errors, psycopg2/conenction.h,
+                # psycopg2 client errors, psycopg2/connection.h,
                 # psycopg2/cursor.h
                 "connection already closed",
                 "cursor already closed",
