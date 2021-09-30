8df30ea9e3f17714ba92aeec23ba5b72120e6b2a
Sumit Khanna <ksumeet40@gmail.com>
2021-09-30 12:30:17 +0530

    initial draft : setting found_table properly so as to also reflect existing tables but with no columns

--- lib/sqlalchemy/engine/reflection.py
+++ lib/sqlalchemy/engine/reflection.py
@@ -768,14 +768,12 @@ class Inspector(object):
             if isinstance(table_name, str):
                 table_name = table_name.decode(dialect.encoding)
 
-        found_table = False
+        found_table = True if table.name in self.engine.table_names() else False
         cols_by_orig_name = {}
 
         for col_d in self.get_columns(
             table_name, schema, **table.dialect_kwargs
         ):
-            found_table = True
-
             self._reflect_column(
                 table,
                 col_d,
