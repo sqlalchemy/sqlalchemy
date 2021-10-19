233ad5c79cab049e4f81a7448e4e47b3d83e45c1
Kevin Kirsche <kevin.kirsche@one.verizon.com>
2021-10-19 10:23:20 -0400

    fix: pep8 line length

--- lib/sqlalchemy/dialects/mysql/reserved_words.py
+++ lib/sqlalchemy/dialects/mysql/reserved_words.py
@@ -1,4 +1,5 @@
-# generated using https://gist.github.com/kkirsche/4f31f2153ed7a3248be1ec44ca6ddbc9
+# generated using:
+# https://gist.github.com/kkirsche/4f31f2153ed7a3248be1ec44ca6ddbc9
 #
 # https://mariadb.com/kb/en/reserved-words/
 # includes: Reserved Words
