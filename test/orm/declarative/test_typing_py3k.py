7db7fd869a6bb617f767fad5b71ddf7cb5f14ff5
Kai Mueller <15907922+kasium@users.noreply.github.com>
2021-12-01 15:17:41 +0000

    Fix tests

--- test/orm/declarative/test_typing_py3k.py
+++ test/orm/declarative/test_typing_py3k.py
@@ -5,19 +5,21 @@ from typing import TypeVar
 from sqlalchemy import Column
 from sqlalchemy import Integer
 from sqlalchemy.orm import as_declarative
+from sqlalchemy.testing import fixtures
 
 
-def test_class_getitem():
-    T = TypeVar("T", bound="CommonBase")  # noqa
+class DeclarativeBaseTest(fixtures.TestBase):
+    def test_class_getitem(self):
+        T = TypeVar("T", bound="CommonBase")  # noqa
 
-    class CommonBase(Generic[T]):
-        @classmethod
-        def boring(cls: Type[T]) -> Type[T]:
-            return cls
+        class CommonBase(Generic[T]):
+            @classmethod
+            def boring(cls: Type[T]) -> Type[T]:
+                return cls
 
-    @as_declarative()
-    class Base(CommonBase[T]):
-        pass
+        @as_declarative()
+        class Base(CommonBase[T]):
+            pass
 
-    class Tab(Base["Tab"]):
-        a = Column(Integer, primary_key=True)
+        class Tab(Base["Tab"]):
+            a = Column(Integer, primary_key=True)
