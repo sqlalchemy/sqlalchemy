e448f979d1ad138f91b8bee5a19f911aed1f2e69
Arie Bovenberg <a.c.bovenberg@gmail.com>
2022-01-21 17:02:50 +0100

    add empty slots to three more base classes

--- lib/sqlalchemy/sql/visitors.py
+++ lib/sqlalchemy/sql/visitors.py
@@ -184,6 +184,8 @@ class _HasTraversalDispatch:
 
     """
 
+    __slots__ = ()
+
     def __init_subclass__(cls) -> None:
         cls._generate_traversal_dispatch()
         super().__init_subclass__()
@@ -298,6 +300,8 @@ class InternalTraversal(_HasTraversalDispatch):
     .. versionadded:: 1.4
 
     """
+
+    __slots__ = ()
 
     dp_has_cache_key = symbol("HC")
     """Visit a :class:`.HasCacheKey` object."""
