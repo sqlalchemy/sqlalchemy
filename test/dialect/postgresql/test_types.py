5c68419e5aee2e27bf21a8ac9eb5950d196c77e5
zeeeeeb <5767468+zeeeeeb@users.noreply.github.com>
2022-02-12 08:56:00 -0800

    fixing linting

--- test/dialect/postgresql/test_types.py
+++ test/dialect/postgresql/test_types.py
@@ -133,7 +133,8 @@ class FloatCoercionTest(fixtures.TablesTest, AssertsExecutionResults):
         )
         metadata.create_all(connection)
         connection.execute(
-            t1.insert(), dict(x=[5], y=[5], z=[6], w=[7], q=[decimal.Decimal("6.4")])
+            t1.insert(),
+            dict(x=[5], y=[5], z=[6], w=[7], q=[decimal.Decimal("6.4")]),
         )
         row = connection.execute(t1.select()).first()
         eq_(row, ([5], [5], [6], [7], [decimal.Decimal("6.4")]))
@@ -150,7 +151,8 @@ class FloatCoercionTest(fixtures.TablesTest, AssertsExecutionResults):
         )
         metadata.create_all(connection)
         connection.execute(
-            t1.insert(), dict(x=[5], y=[5], z=[6], w=[7], q=[decimal.Decimal("6.4")])
+            t1.insert(),
+            dict(x=[5], y=[5], z=[6], w=[7], q=[decimal.Decimal("6.4")]),
         )
         row = connection.execute(t1.select()).first()
         eq_(row, ([5], [5], [6], [7], [decimal.Decimal("6.4")]))
