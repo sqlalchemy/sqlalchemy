25fa4c0250730958c39676007cc39df7b0ee2124
Mike Bayer <mike_mp@zzzcomputing.com>
2022-08-22 13:55:08 -0400

    more function adjustments
    the inherited-members feature works very poorly
    and inconsistently in sphinx.  just dont use it here as it
    refuses to exclude ColumnOperators methods
    
    Change-Id: Ic50865c9901e7225a99ff7f33454da15ff91b12f

--- lib/sqlalchemy/sql/functions.py
+++ lib/sqlalchemy/sql/functions.py
@@ -572,7 +572,7 @@ class FunctionElement(Executable, ColumnElement[_T], FromClause, Generative):
         in the columns or where clause, for a backend such as PostgreSQL.
 
         For a full table-valued expression, use the
-        :meth:`_function.FunctionElement.table_valued` method first to
+        :meth:`_functions.FunctionElement.table_valued` method first to
         establish named columns.
 
         e.g.::
