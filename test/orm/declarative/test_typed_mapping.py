c35921b79e1683d3738a69f8ff6a892c3c2846e2
Mike Bayer <mike_mp@zzzcomputing.com>
2022-10-10 14:03:04 -0400

    warn for no polymorphic identity w/ poly hierarchy
    A warning is emitted when attempting to configure a mapped class within an
    inheritance hierarchy where the mapper is not given any polymorphic
    identity, however there is a polymorphic discriminator column assigned.
    Such classes should be abstract if they never intend to load directly.
    
    Fixes: #7545
    Change-Id: I94f04e59736c73e3f39d883a75d763e3f06ecc3d

--- test/orm/declarative/test_typed_mapping.py
+++ test/orm/declarative/test_typed_mapping.py
@@ -1822,6 +1822,7 @@ class AllYourFavoriteHitsTest(fixtures.TestBase, testing.AssertsCompiledSQL):
             person_id: Mapped[int] = mapped_column(
                 ForeignKey("person.person_id"), primary_key=True
             )
+            __mapper_args__ = {"polymorphic_identity": "engineer"}
 
             status: Mapped[str] = mapped_column(String(30))
             engineer_name: Mapped[opt_str50]
@@ -1835,6 +1836,7 @@ class AllYourFavoriteHitsTest(fixtures.TestBase, testing.AssertsCompiledSQL):
             )
             status: Mapped[str] = mapped_column(String(30))
             manager_name: Mapped[str50]
+            __mapper_args__ = {"polymorphic_identity": "manager"}
 
         is_(Person.__mapper__.polymorphic_on, Person.__table__.c.type)
 
