d885bbdbf885a354fa1c0f4a09aa6fb7367f6aeb
Heckad <heckad@yandex.ru>
2022-10-20 12:58:39 +0300

    Fix test

--- test/ext/mypy/plugin_files/ensure_descriptor_type_fully_inferred.py
+++ test/ext/mypy/plugin_files/ensure_descriptor_type_fully_inferred.py
@@ -12,9 +12,11 @@ class User:
 
     id = Column(Integer(), primary_key=True)
     name = Column(String, nullable=False)
+    middle_name = Column(String, nullable=True)
 
 
 u1 = User()
 
+v: str = u1.name
 # EXPECTED_MYPY: Incompatible types in assignment (expression has type "Optional[str]", variable has type "str")  # noqa: E501
-p: str = u1.name
+p: str = u1.middle_name
