59f2d0c96152128d8e02e43dcda045593153c86e
Mike Barry <michael.barry@gmo.com>
2022-10-26 13:40:32 -0400

    use only object_id() function for temp tables
    Fixed issue with :meth:`.Inspector.has_table` when used against a temporary
    table for the SQL Server dialect would fail an invalid object name error on
    some Azure variants, due to an unnecessary information schema query that is
    not supported on those server versions. Pull request courtesy Mike Barry.
    
    the patch also fills out test support for has_table()
    against temp tables, temp views, adding to the has_table() support just
    added for views in #8700.
    
    Fixes: #8714
    Closes: #8716
    Pull-request: https://github.com/sqlalchemy/sqlalchemy/pull/8716
    Pull-request-sha: e2ac7a52e2b09a349a703ba1e1a2911f4d3c0912
    
    Change-Id: Ia73e4e9e977a2d6b7e100abd2f81a8c8777dc9bb

--- test/requirements.py
+++ test/requirements.py
@@ -610,6 +610,18 @@ class DefaultRequirements(SuiteRequirements):
         )
 
     @property
+    def has_temp_table(self):
+        """target dialect supports checking a single temp table name
+
+        unfortunately this is not the same as temp_table_names
+
+        """
+
+        return only_on(["sqlite", "oracle", "postgresql", "mssql"]) + skip_if(
+            self._sqlite_file_db
+        )
+
+    @property
     def temporary_views(self):
         """target database supports temporary views"""
         return only_on(["sqlite", "postgresql"]) + skip_if(
