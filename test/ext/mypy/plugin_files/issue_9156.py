109af6eeccafe51048ef82268582e67593b455c7
Yurii Karabas <1998uriyyo@gmail.com>
2023-01-27 14:31:30 +0200

    Add more cases

--- test/ext/mypy/plugin_files/issue_9156.py
+++ test/ext/mypy/plugin_files/issue_9156.py
@@ -3,7 +3,9 @@ from typing import Type, Any
 from sqlalchemy.sql.elements import ColumnElement
 from sqlalchemy.sql.type_api import TypeEngine
 
-col: ColumnElement
+col: ColumnElement[Any]
 type_: Type[TypeEngine[Any]]
+obj: TypeEngine[Any]
 
 col.cast(type_)
+col.cast(obj)
