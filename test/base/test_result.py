sqlalchemy/sqlalchemy

commit a4bb502cf95ea3523e4d383c4377e50f402d7d52

pep-484 for engine
All modules in sqlalchemy.engine are strictly
typed with the exception of cursor, default, and
reflection.  cursor and default pass with non-strict
typing, reflection is waiting on the multi-reflection
refactor.

Behavioral changes:

* create_connect_args() methods return a tuple of list,
  dict, rather than a list of list, dict
* removed allow_chars parameter from
  pyodbc connector ._get_server_version_info()
  method
* the parameter list passed to do_executemany is now
  a list in all cases. previously, this was being run
  through dialect.execute_sequence_format, which
  defaults to tuple and was only intended for individual
  tuple params.
* broke up dialect.dbapi into dialect.import_dbapi
  class method and dialect.dbapi module object.  added
  a deprecation path for legacy dialects.  it's not
  really feasible to type a single attr as a classmethod
  vs. module type.  The "type_compiler" attribute also
  has this problem with greater ability to work around,
  left that one for now.
* lots of constants changing to be Enum, so that we can
  type them.  for fixed tuple-position constants in
  cursor.py / compiler.py (which are used to avoid the
  speed overhead of namedtuple), using Literal[value]
  which seems to work well
* some tightening up in Row regarding __getitem__, which
  we can do since we are on full 2.0 style result use
* altered the set_connection_execution_options and
  set_engine_execution_options event flows so that the
  dictionary of options may be mutated within the event
  hook, where it will then take effect as the actual
  options used.  Previously, changing the dict would
  be silently ignored which seems counter-intuitive
  and not very useful.
* A lot of DefaultDialect/DefaultExecutionContext
  methods and attributes, including underscored ones, move
  to interfaces.  This is not fully ideal as it means
  the Dialect/ExecutionContext interfaces aren't publicly
  subclassable directly, but their current purpose

lib/sqlalchemy/engine/_py_row.py
@@ -1,26 +1,59 @@
 from __future__ import annotations
 
+import enum
 import operator
+import typing
+from typing import Any
+from typing import Callable
+from typing import Dict
+from typing import Iterator
+from typing import List
+from typing import Optional
+from typing import Tuple
+from typing import Type
+from typing import Union
+
+if typing.TYPE_CHECKING:
+    from .result import _KeyMapType
+    from .result import _KeyType
+    from .result import _ProcessorsType
+    from .result import _RawRowType
+    from .result import _TupleGetterType
+    from .result import ResultMetaData
 
 MD_INDEX = 0  # integer index in cursor.description
 
-KEY_INTEGER_ONLY = 0
-"""__getitem__ only allows integer values and slices, raises TypeError
-   otherwise"""
 
-KEY_OBJECTS_ONLY = 1
-"""__getitem__ only allows string/object values, raises TypeError otherwise"""
+class _KeyStyle(enum.Enum):
+    KEY_INTEGER_ONLY = 0
+    """__getitem__ only allows integer values and slices, raises TypeError
+    otherwise"""
 
-sqlalchemy_engine_row = None
+    KEY_OBJECTS_ONLY = 1
+    """__getitem__ only allows string/object values, raises TypeError
+    otherwise"""
+
+
+KEY_INTEGER_ONLY, KEY_OBJECTS_ONLY = list(_KeyStyle)
 
 
 class BaseRow:
-    Row = None
     __slots__ = ("_parent", "_data", "_keymap", "_key_style")
 
-    def __init__(self, parent, processors, keymap, key_style, data):
-        """Row objects are constructed by CursorResult objects."""
+    _parent: ResultMetaData
+    _data: _RawRowType
+    _keymap: _KeyMapType
+    _key_style: _KeyStyle
 
+    def __init__(
+        self,
+        parent: ResultMetaData,
+        processors: Optional[_ProcessorsType],
+        keymap: _KeyMapType,
+        key_style: _KeyStyle,
+        data: _RawRowType,
+    ):
+        """Row objects are constructed by CursorResult objects."""
         object.__setattr__(self, "_parent", parent)
 
         if processors:
@@ -41,68 +74,45 @@ class BaseRow:
 
         object.__setattr__(self, "_key_style", key_style)
 
-    def __reduce__(self):
+    def __reduce__(self) -> Tuple[Callable[..., BaseRow], Tuple[Any, ...]]:
         return (
             rowproxy_reconstructor,
             (self.__class__, self.__getstate__()),
         )
 
-    def __getstate__(self):
+    def __getstate__(self) -> Dict[str, Any]:
         return {
             "_parent": self._parent,
             "_data": self._data,
             "_key_style": self._key_style,
         }
 
-    def __setstate__(self, state):
+    def __setstate__(self, state: Dict[str, Any]) -> None:
         parent = state["_parent"]
         object.__setattr__(self, "_parent", parent)
         object.__setattr__(self, "_data", state["_data"])
         object.__setattr__(self, "_keymap", parent._keymap)
         object.__setattr__(self, "_key_style", state["_key_style"])
 
-    def _filter_on_values(self, filters):
-        global sqlalchemy_engine_row
-        if sqlalchemy_engine_row is None:
-            from sqlalchemy.engine.row import Row as sqlalchemy_engine_row
-
-        return sqlalchemy_engine_row(
-            self._parent,
-            filters,
-            self._keymap,
-            self._key_style,
-            self._data,
-        )
-
-    def _values_impl(self):
+    def _values_impl(self) -> List[Any]:
         return list(self)
 
-    def __iter__(self):
+    def __iter__(self) -> Iterator[Any]:
         return iter(self._data)
 
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self._data)
 
-    def __hash__(self):
+    def __hash__(self) -> int:
         return hash(self._data)
 
-    def _get_by_int_impl(self, key):
+    def _get_by_int_impl(self, key: Union[int, slice]) -> Any:
         return self._data[key]
 
-    def _get_by_key_impl(self, key):
-        # keep two isinstance since it's noticeably faster in the int case
-        if isinstance(key, int) or isinstance(key, slice):
-            return self._data[key]
+    if not typing.TYPE_CHECKING:
+        __getitem__ = _get_by_int_impl
 
-        self._parent._raise_for_nonint(key)
-
-    # The original 1.4 plan was that Row would not allow row["str"]
-    # access, however as the C extensions were inadvertently allowing
-    # this coupled with the fact that orm Session sets future=True,
-    # this allows a softer upgrade path.  see #6218
-    __getitem__ = _get_by_key_impl
-
-    def _get_by_key_impl_mapping(self, key):
+    def _get_by_key_impl_mapping(self, key: _KeyType) -> Any:
         try:
             rec = self._keymap[key]
         except KeyError as ke:
@@ -116,7 +126,7 @@ class BaseRow:
 
         return self._data[mdindex]
 
-    def __getattr__(self, name):
+    def __getattr__(self, name: str) -> Any:
         try:
             return self._get_by_key_impl_mapping(name)
         except KeyError as e:
@@ -125,13 +135,15 @@ class BaseRow:
 
 # This reconstructor is necessary so that pickles with the Cy extension or
 # without use the same Binary format.
-def rowproxy_reconstructor(cls, state):
+def rowproxy_reconstructor(
+    cls: Type[BaseRow], state: Dict[str, Any]
+) -> BaseRow:
     obj = cls.__new__(cls)
     obj.__setstate__(state)
     return obj
 
 
-def tuplegetter(*indexes):
+def tuplegetter(*indexes: int) -> _TupleGetterType:
     it = operator.itemgetter(*indexes)
 
     if len(indexes) > 1:

test/base/test_result.py
@@ -1,7 +1,6 @@
 from sqlalchemy import exc
 from sqlalchemy import testing
 from sqlalchemy.engine import result
-from sqlalchemy.engine.row import Row
 from sqlalchemy.testing import assert_raises
 from sqlalchemy.testing import assert_raises_message
 from sqlalchemy.testing import eq_
@@ -727,27 +726,6 @@ class ResultTest(fixtures.TestBase):
 
         # still slices
         eq_(m1.fetchone(), {"b": 1, "c": 2})
-
-    def test_alt_row_fetch(self):
-        class AppleRow(Row):
-            def apple(self):
-                return "apple"
-
-        result = self._fixture(alt_row=AppleRow)
-
-        row = result.all()[0]
-        eq_(row.apple(), "apple")
-
-    def test_alt_row_transform(self):
-        class AppleRow(Row):
-            def apple(self):
-                return "apple"
-
-        result = self._fixture(alt_row=AppleRow)
-
-        row = result.columns("c", "a").all()[2]
-        eq_(row.apple(), "apple")
-        eq_(row, (2, 1))
 
     def test_scalar_none_iterate(self):
         result = self._fixture(

test/sql/test_resultset.py
@@ -1612,15 +1612,15 @@ class CursorResultTest(fixtures.TablesTest):
 
         eq_(dict(row._mapping), {"a": "av", "b": "bv", "count": "cv"})
 
-        with assertions.expect_raises_message(
+        with assertions.expect_raises(
             TypeError,
-            "TypeError: tuple indices must be integers or slices, not str",
+            "tuple indices must be integers or slices, not str",
         ):
             eq_(row["a"], "av")
 
         with assertions.expect_raises_message(
             TypeError,
-            "TypeError: tuple indices must be integers or slices, not str",
+            "tuple indices must be integers or slices, not str",
         ):
             eq_(row["count"], "cv")
 
@@ -3197,8 +3197,7 @@ class GenerativeResultTest(fixtures.TablesTest):
         all_ = result.columns(*columns).all()
         eq_(all_, expected)
 
-        # ensure Row / LegacyRow comes out with .columns
-        assert type(all_[0]) is result._process_row
+        assert type(all_[0]) is Row
 
     def test_columns_twice(self, connection):
         users = self.tables.users
@@ -3216,8 +3215,7 @@ class GenerativeResultTest(fixtures.TablesTest):
         )
         eq_(all_, [("jack", 1)])
 
-        # ensure Row / LegacyRow comes out with .columns
-        assert type(all_[0]) is result._process_row
+        assert type(all_[0]) is Row
 
     def test_columns_plus_getter(self, connection):
         users = self.tables.users

lib/sqlalchemy/engine/util.py
@@ -7,18 +7,30 @@
 
 from __future__ import annotations
 
+import typing
+from typing import Any
+from typing import Callable
+from typing import TypeVar
+
 from .. import exc
 from .. import util
+from ..util._has_cy import HAS_CYEXTENSION
 
-try:
-    from sqlalchemy.cyextension.util import _distill_params_20  # noqa
-    from sqlalchemy.cyextension.util import _distill_raw_params  # noqa
-except ImportError:
-    from ._py_util import _distill_params_20  # noqa
-    from ._py_util import _distill_raw_params  # noqa
+if typing.TYPE_CHECKING or not HAS_CYEXTENSION:
+    from ._py_util import _distill_params_20 as _distill_params_20
+    from ._py_util import _distill_raw_params as _distill_raw_params
+else:
+    from sqlalchemy.cyextension.util import (
+        _distill_params_20 as _distill_params_20,
+    )
+    from sqlalchemy.cyextension.util import (
+        _distill_raw_params as _distill_raw_params,
+    )
+
+_C = TypeVar("_C", bound=Callable[[], Any])
 
 
-def connection_memoize(key):
+def connection_memoize(key: str) -> Callable[[_C], _C]:
     """Decorator, memoize a function in a connection.info stash.
 
     Only applicable to functions which take no arguments other than a
@@ -26,7 +38,7 @@ def connection_memoize(key):
     """
 
     @util.decorator
-    def decorated(fn, self, connection):
+    def decorated(fn, self, connection):  # type: ignore
         connection = connection.connect()
         try:
             return connection.info[key]
@@ -34,7 +46,7 @@ def connection_memoize(key):
             connection.info[key] = val = fn(self, connection)
             return val
 
-    return decorated
+    return decorated  # type: ignore[return-value]
 
 
 class TransactionalContext:
@@ -47,13 +59,13 @@ class TransactionalContext:
 
     __slots__ = ("_outer_trans_ctx", "_trans_subject", "__weakref__")
 
-    def _transaction_is_active(self):
+    def _transaction_is_active(self) -> bool:
         raise NotImplementedError()
 
-    def _transaction_is_closed(self):
+    def _transaction_is_closed(self) -> bool:
         raise NotImplementedError()
 
-    def _rollback_can_be_called(self):
+    def _rollback_can_be_called(self) -> bool:
         """indicates the object is in a state that is known to be acceptable
         for rollback() to be called.
 
@@ -70,11 +82,20 @@ class TransactionalContext:
         """
         raise NotImplementedError()
 
-    def _get_subject(self):
+    def _get_subject(self) -> Any:
+        raise NotImplementedError()
+
+    def commit(self) -> None:
+        raise NotImplementedError()
+
+    def rollback(self) -> None:
+        raise NotImplementedError()
+
+    def close(self) -> None:
         raise NotImplementedError()
 
     @classmethod
-    def _trans_ctx_check(cls, subject):
+    def _trans_ctx_check(cls, subject: Any) -> None:
         trans_context = subject._trans_context_manager
         if trans_context:
             if not trans_context._transaction_is_active():
@@ -84,7 +105,7 @@ class TransactionalContext:
                     "before emitting further commands."
                 )
 
-    def __enter__(self):
+    def __enter__(self) -> TransactionalContext:
         subject = self._get_subject()
 
         # none for outer transaction, may be non-None for nested
@@ -96,7 +117,7 @@ class TransactionalContext:
         subject._trans_context_manager = self
         return self
 
-    def __exit__(self, type_, value, traceback):
+    def __exit__(self, type_: Any, value: Any, traceback: Any) -> None:
         subject = getattr(self, "_trans_subject", None)
 
         # simplistically we could assume that
@@ -119,6 +140,7 @@ class TransactionalContext:
                         self.rollback()
             finally:
                 if not out_of_band_exit:
+                    assert subject is not None
                     subject._trans_context_manager = self._outer_trans_ctx
                 self._trans_subject = self._outer_trans_ctx = None
         else:
@@ -131,5 +153,6 @@ class TransactionalContext:
                         self.rollback()
             finally:
                 if not out_of_band_exit:
+                    assert subject is not None
                     subject._trans_context_manager = self._outer_trans_ctx
                 self._trans_subject = self._outer_trans_ctx = None

lib/sqlalchemy/sql/util.py
@@ -21,9 +21,6 @@ from . import coercions
 from . import operators
 from . import roles
 from . import visitors
-from ._typing import _ExecuteParams
-from ._typing import _MultiExecuteParams
-from ._typing import _SingleExecuteParams
 from .annotation import _deep_annotate  # noqa
 from .annotation import _deep_deannotate  # noqa
 from .annotation import _shallow_annotate  # noqa
@@ -54,6 +51,10 @@ from .. import exc
 from .. import util
 
 if typing.TYPE_CHECKING:
+    from ..engine.interfaces import _AnyExecuteParams
+    from ..engine.interfaces import _AnyMultiExecuteParams
+    from ..engine.interfaces import _AnySingleExecuteParams
+    from ..engine.interfaces import _CoreSingleExecuteParams
     from ..engine.row import Row
 
 
@@ -550,12 +551,12 @@ class _repr_params(_repr_base):
 
     def __init__(
         self,
-        params: _ExecuteParams,
+        params: Optional[_AnyExecuteParams],
         batches: int,
         max_chars: int = 300,
         ismulti: Optional[bool] = None,
     ):
-        self.params: _ExecuteParams = params
+        self.params = params
         self.ismulti = ismulti
         self.batches = batches
         self.max_chars = max_chars
@@ -575,7 +576,10 @@ class _repr_params(_repr_base):
             return self.trunc(self.params)
 
         if self.ismulti:
-            multi_params = cast(_MultiExecuteParams, self.params)
+            multi_params = cast(
+                "_AnyMultiExecuteParams",
+                self.params,
+            )
 
             if len(self.params) > self.batches:
                 msg = (
@@ -595,10 +599,18 @@ class _repr_params(_repr_base):
                 return self._repr_multi(multi_params, typ)
         else:
             return self._repr_params(
-                cast(_SingleExecuteParams, self.params), typ
+                cast(
+                    "_AnySingleExecuteParams",
+                    self.params,
+                ),
+                typ,
             )
 
-    def _repr_multi(self, multi_params: _MultiExecuteParams, typ) -> str:
+    def _repr_multi(
+        self,
+        multi_params: _AnyMultiExecuteParams,
+        typ,
+    ) -> str:
         if multi_params:
             if isinstance(multi_params[0], list):
                 elem_type = self._LIST
@@ -622,13 +634,19 @@ class _repr_params(_repr_base):
         else:
             return "(%s)" % elements
 
-    def _repr_params(self, params: _SingleExecuteParams, typ: int) -> str:
+    def _repr_params(
+        self,
+        params: Optional[_AnySingleExecuteParams],
+        typ: int,
+    ) -> str:
         trunc = self.trunc
         if typ is self._DICT:
             return "{%s}" % (
                 ", ".join(
                     "%r: %s" % (key, trunc(value))
-                    for key, value in params.items()
+                    for key, value in cast(
+                        "_CoreSingleExecuteParams", params
+                    ).items()
                 )
             )
         elif typ is self._TUPLE:

commit fd9aa847920b9e4dff61ef7a5555c9fa6e362484

Fix regression when deserializing python rows into cython
Fixed regression involving pickling of Python rows between the cython and
pure Python implementations of :class:`.Row`, which occurred as part of
refactoring code for version 2.0 with typing. A particular constant were
turned into a string based ``Enum`` for the pure Python version of
:class:`.Row` whereas the cython version continued to use an integer
constant, leading to deserialization failures.

Regression occurred in a4bb502cf95ea3523e4d383c4377e50f402d7d52

Fixes: #9423
Change-Id: Icbd85cacb2d589cef7c246de7064249926146f2e

lib/sqlalchemy/engine/_py_row.py
@@ -24,7 +24,7 @@ if typing.TYPE_CHECKING:
 MD_INDEX = 0  # integer index in cursor.description
 
 
-class _KeyStyle(enum.Enum):
+class _KeyStyle(enum.IntEnum):
     KEY_INTEGER_ONLY = 0
     """__getitem__ only allows integer values and slices, raises TypeError
     otherwise"""
@@ -121,6 +121,9 @@ class BaseRow:
         mdindex = rec[MD_INDEX]
         if mdindex is None:
             self._parent._raise_for_ambiguous_column_name(rec)
+        # NOTE: keep "== KEY_OBJECTS_ONLY" instead of "is KEY_OBJECTS_ONLY"
+        # since deserializing the class from cython will load an int in
+        # _key_style, not an instance of _KeyStyle
         elif self._key_style == KEY_OBJECTS_ONLY and isinstance(key, int):
             raise KeyError(key)
 

lib/sqlalchemy/testing/util.py
@@ -59,7 +59,7 @@ def picklers():
 
     # yes, this thing needs this much testing
     for pickle_ in picklers:
-        for protocol in range(-2, pickle.HIGHEST_PROTOCOL):
+        for protocol in range(-2, pickle.HIGHEST_PROTOCOL + 1):
             yield pickle_.loads, lambda d: pickle_.dumps(d, protocol)
 
 

test/base/test_result.py
@@ -195,35 +195,48 @@ class ResultTupleTest(fixtures.TestBase):
             eq_(kt._asdict(), {"a": 1, "b": 3})
 
     @testing.requires.cextensions
-    def test_serialize_cy_py_cy(self):
-        from sqlalchemy.engine._py_row import BaseRow as _PyRow
-        from sqlalchemy.cyextension.resultproxy import BaseRow as _CyRow
+    @testing.variation("direction", ["py_to_cy", "cy_to_py"])
+    def test_serialize_cy_py_cy(self, direction: testing.Variation):
+        from sqlalchemy.engine import _py_row
+        from sqlalchemy.cyextension import resultproxy as _cy_row
 
         global Row
 
-        p = result.SimpleResultMetaData(["a", None, "b"])
+        p = result.SimpleResultMetaData(["a", "w", "b"])
+
+        if direction.py_to_cy:
+            dump_cls = _py_row.BaseRow
+            num = _py_row.KEY_INTEGER_ONLY
+            load_cls = _cy_row.BaseRow
+        elif direction.cy_to_py:
+            dump_cls = _cy_row.BaseRow
+            num = _cy_row.KEY_INTEGER_ONLY
+            load_cls = _py_row.BaseRow
+        else:
+            direction.fail()
 
         for loads, dumps in picklers():
 
-            class Row(_CyRow):
+            class Row(dump_cls):
                 pass
 
-            row = Row(p, p._processors, p._keymap, 0, (1, 2, 3))
+            row = Row(p, p._processors, p._keymap, num, (1, 2, 3))
 
             state = dumps(row)
 
-            class Row(_PyRow):
+            class Row(load_cls):
                 pass
 
             row2 = loads(state)
-            is_true(isinstance(row2, _PyRow))
+            is_true(isinstance(row2, load_cls))
+            is_false(isinstance(row2, dump_cls))
             state2 = dumps(row2)
 
-            class Row(_CyRow):
+            class Row(dump_cls):
                 pass
 
             row3 = loads(state2)
-            is_true(isinstance(row3, _CyRow))
+            is_true(isinstance(row3, dump_cls))
 
 
 class ResultTest(fixtures.TestBase):

test/base/test_utils.py
@@ -2458,9 +2458,8 @@ class SymbolTest(fixtures.TestBase):
         s = pickle.dumps(sym1)
         pickle.loads(s)
 
-        for protocol in 0, 1, 2:
-            print(protocol)
-            serial = pickle.dumps(sym1)
+        for _, dumper in picklers():
+            serial = dumper(sym1)
             rt = pickle.loads(serial)
             assert rt is sym1
             assert rt is sym2

test/sql/test_resultset.py
@@ -4,7 +4,11 @@ from contextlib import contextmanager
 import csv
 from io import StringIO
 import operator
+import os
 import pickle
+import subprocess
+import sys
+from tempfile import mkstemp
 from unittest.mock import Mock
 from unittest.mock import patch
 
@@ -502,25 +506,25 @@ class CursorResultTest(fixtures.TablesTest):
             lambda: result[0]._mapping[addresses.c.address_id],
         )
 
-    # @testing.variation("use_labels", [True, False])
-    # def _dont_test_pickle_rows_other_process(self, connection, use_labels):
-    #     result = self._pickle_row_data(connection, use_labels)
+    @testing.variation("use_labels", [True, False])
+    def test_pickle_rows_other_process(self, connection, use_labels):
+        result = self._pickle_row_data(connection, use_labels)
 
-    #     f, name = mkstemp("pkl")
-    #     with os.fdopen(f, "wb") as f:
-    #         pickle.dump(result, f)
-    #     name = name.replace(os.sep, "/")
-    #     code = (
-    #         "import sqlalchemy; import pickle; print(["
-    #         f"r[0] for r in pickle.load(open('''{name}''', 'rb'))])"
-    #     )
-    #     proc = subprocess.run(
-    #         [sys.executable, "-c", code], stdout=subprocess.PIPE
-    #     )
-    #     exp = str([r[0] for r in result]).encode()
-    #     eq_(proc.returncode, 0)
-    #     eq_(proc.stdout.strip(), exp)
-    #     os.unlink(name)
+        f, name = mkstemp("pkl")
+        with os.fdopen(f, "wb") as f:
+            pickle.dump(result, f)
+        name = name.replace(os.sep, "/")
+        code = (
+            "import sqlalchemy; import pickle; print(["
+            f"r[0] for r in pickle.load(open('''{name}''', 'rb'))])"
+        )
+        proc = subprocess.run(
+            [sys.executable, "-c", code], stdout=subprocess.PIPE
+        )
+        exp = str([r[0] for r in result]).encode()
+        eq_(proc.returncode, 0)
+        eq_(proc.stdout.strip(), exp)
+        os.unlink(name)
 
     def test_column_error_printing(self, connection):
         result = connection.execute(select(1))