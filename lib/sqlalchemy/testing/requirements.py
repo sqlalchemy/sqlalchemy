130f6a9f1b4d50b2d4ac571a6f846101185d8e12
F.D.Castel <fdcastel@gmail.com>
2023-05-05 18:40:23 -0300

    Fix typos.

--- lib/sqlalchemy/testing/requirements.py
+++ lib/sqlalchemy/testing/requirements.py
@@ -549,7 +549,7 @@ class SuiteRequirements(Requirements):
 
     @property
     def foreign_key_constraint_name_reflection(self):
-        """Target supports refleciton of FOREIGN KEY constraints and
+        """Target supports reflection of FOREIGN KEY constraints and
         will return the name of the constraint that was used in the
         "CONSTRAINT <name> FOREIGN KEY" DDL.
 
@@ -664,7 +664,7 @@ class SuiteRequirements(Requirements):
 
     @property
     def constraint_comment_reflection(self):
-        """indicates if the database support constraint on constraints
+        """indicates if the database support comments on constraints
         and their reflection"""
         return exclusions.closed()
 
