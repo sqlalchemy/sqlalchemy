8e465ce2788a726c614c060c11ae77b05d0b99af
Mike Bayer <mike_mp@zzzcomputing.com>
2023-05-19 09:50:09 -0400

    clarify exec_driver_sql
    this docstring wasn't really saying what this method was for.
    
    Change-Id: I2d83ed2690c93d215faadaf7c6edcb02c6c57a6f
    (cherry picked from commit 75d716c7799e28ba5c86c72db08025dd6fc8c6e0)

--- lib/sqlalchemy/engine/base.py
+++ lib/sqlalchemy/engine/base.py
@@ -1712,8 +1712,11 @@ class Connection(Connectable):
     def exec_driver_sql(
         self, statement, parameters=None, execution_options=None
     ):
-        r"""Executes a SQL statement construct and returns a
-        :class:`_engine.CursorResult`.
+        r"""Executes a string SQL statement on the DBAPI cursor directly,
+        without any SQL compilation steps.
+
+        This can be used to pass any string directly to the
+        ``cursor.execute()`` method of the DBAPI in use.
 
         :param statement: The statement str to be executed.   Bound parameters
          must use the underlying DBAPI's paramstyle, such as "qmark",
@@ -1723,6 +1726,8 @@ class Connection(Connectable):
          execution.  The format is one of:   a dictionary of named parameters,
          a tuple of positional parameters, or a list containing either
          dictionaries or tuples for multiple-execute support.
+
+        :return: a :class:`_engine.CursorResult`.
 
          E.g. multiple dictionaries::
 
