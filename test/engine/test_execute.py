0348b5d72d47f22a3d63dd6916c27571d9d6830a
semen603089 <semen603089@mail.ru>
2023-07-09 09:12:41 +0300

    tests add

--- test/engine/test_execute.py
+++ test/engine/test_execute.py
@@ -130,6 +130,12 @@ class ExecuteTest(fixtures.TablesTest):
             [[3, "ed"], [4, "horse"]],
         )
 
+    def test_create_engine_url_invalid(self):
+        assert_raises_message(tsa.exc.ArgumentError,
+                              "SQLAlchemy URL must be of string/URL/Mock type, given: <class 'int'>",
+                              create_engine,
+                              42)
+
     def test_raw_named_invalid(self, connection):
         # this is awkward b.c. this is just testing if regular Python
         # is raising TypeError if they happened to send arguments that
