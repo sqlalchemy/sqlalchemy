fa07ce93ff3f8ba1afdb29bf875d14e74f07abd9
Mehdi Gmira <mgmira@wiremind.io>
2023-07-15 18:33:55 +0200

    query

--- lib/sqlalchemy/orm/query.py
+++ lib/sqlalchemy/orm/query.py
@@ -22,124 +22,129 @@ from __future__ import annotations
 
 import collections.abc as collections_abc
 import operator
-from typing import (
-    TYPE_CHECKING,
-    Any,
-    Callable,
-    Dict,
-    Generic,
-    Iterable,
-    Iterator,
-    List,
-    Mapping,
-    Optional,
-    Sequence,
-    Tuple,
-    Type,
-    TypeVar,
-    Union,
-    cast,
-    overload,
-)
+from typing import Any
+from typing import Callable
+from typing import cast
+from typing import Dict
+from typing import Generic
+from typing import Iterable
+from typing import Iterator
+from typing import List
+from typing import Mapping
+from typing import Optional
+from typing import overload
+from typing import Sequence
+from typing import Tuple
+from typing import Type
+from typing import TYPE_CHECKING
+from typing import TypeVar
+from typing import Union
 
-from .. import exc as sa_exc
-from .. import inspect, inspection, log, sql, util
-from ..engine import Result, Row
-from ..event import EventTarget, dispatcher
-from ..sql import Select, coercions, expression, roles
-from ..sql import util as sql_util
-from ..sql import visitors
-from ..sql._typing import _TP, _FromClauseArgument
-from ..sql.annotation import SupportsCloneAnnotations
-from ..sql.base import (
-    Executable,
-    Generative,
-    _entity_namespace_key,
-    _generative,
-    _NoArg,
-)
-from ..sql.elements import BooleanClauseList
-from ..sql.expression import Exists
-from ..sql.selectable import (
-    LABEL_STYLE_TABLENAME_PLUS_COL,
-    ForUpdateArg,
-    HasHints,
-    HasPrefixes,
-    HasSuffixes,
-    SelectLabelStyle,
-    _MemoizedSelectEntities,
-    _SelectFromElements,
-)
-from ..util.typing import Literal, Self
-from . import attributes, interfaces, loading
+from . import attributes
+from . import interfaces
+from . import loading
 from . import util as orm_util
 from ._typing import _O
 from .base import _assertions
-from .context import (
-    FromStatement,
-    ORMCompileState,
-    QueryContext,
-    _column_descriptions,
-    _determine_last_joined_entity,
-    _legacy_filter_by_entity_zero,
-)
-from .interfaces import ORMColumnDescription, ORMColumnsClauseRole
-from .util import AliasedClass, object_mapper, with_parent
+from .context import _column_descriptions
+from .context import _determine_last_joined_entity
+from .context import _legacy_filter_by_entity_zero
+from .context import FromStatement
+from .context import ORMCompileState
+from .context import QueryContext
+from .interfaces import ORMColumnDescription
+from .interfaces import ORMColumnsClauseRole
+from .util import AliasedClass
+from .util import object_mapper
+from .util import with_parent
+from .. import exc as sa_exc
+from .. import inspect
+from .. import inspection
+from .. import log
+from .. import sql
+from .. import util
+from ..engine import Result
+from ..engine import Row
+from ..event import dispatcher
+from ..event import EventTarget
+from ..sql import coercions
+from ..sql import expression
+from ..sql import roles
+from ..sql import Select
+from ..sql import util as sql_util
+from ..sql import visitors
+from ..sql._typing import _FromClauseArgument
+from ..sql._typing import _TP
+from ..sql.annotation import SupportsCloneAnnotations
+from ..sql.base import _entity_namespace_key
+from ..sql.base import _generative
+from ..sql.base import _NoArg
+from ..sql.base import Executable
+from ..sql.base import Generative
+from ..sql.elements import BooleanClauseList
+from ..sql.expression import Exists
+from ..sql.selectable import _MemoizedSelectEntities
+from ..sql.selectable import _SelectFromElements
+from ..sql.selectable import ForUpdateArg
+from ..sql.selectable import HasHints
+from ..sql.selectable import HasPrefixes
+from ..sql.selectable import HasSuffixes
+from ..sql.selectable import LABEL_STYLE_TABLENAME_PLUS_COL
+from ..sql.selectable import SelectLabelStyle
+from ..util.typing import Literal
+from ..util.typing import Self
+
 
 if TYPE_CHECKING:
-    from ..engine.cursor import CursorResult
-    from ..engine.interfaces import (
-        CompiledCacheType,
-        IsolationLevel,
-        SchemaTranslateMapType,
-        _ImmutableExecuteOptions,
-    )
-    from ..engine.result import FrozenResult, ScalarResult
-    from ..sql._typing import (
-        _MAYBE_ENTITY,
-        _NOT_ENTITY,
-        _T0,
-        _T1,
-        _T2,
-        _T3,
-        _T4,
-        _T5,
-        _T6,
-        _T7,
-        _ColumnExpressionArgument,
-        _ColumnExpressionOrStrLabelArgument,
-        _ColumnsClauseArgument,
-        _DMLColumnArgument,
-        _JoinTargetArgument,
-        _LimitOffsetType,
-        _no_kw,
-        _OnClauseArgument,
-        _PropagateAttrsType,
-    )
-    from ..sql._typing import _TypedColumnClauseArgument as _TCCA
-    from ..sql.base import CacheableOptions, ExecutableOption
-    from ..sql.elements import ColumnElement, Label
-    from ..sql.selectable import (
-        CTE,
-        Alias,
-        ExecutableReturnsRows,
-        FromClause,
-        ScalarSelect,
-        Subquery,
-        _JoinTargetElement,
-        _SetupJoinsElement,
-    )
-    from ._typing import (
-        SynchronizeSessionArgument,
-        _EntityType,
-        _ExternalEntityType,
-        _InternalEntityType,
-    )
-    from .decl_api import DeclarativeBase
+    from ._typing import _EntityType
+    from ._typing import _ExternalEntityType
+    from ._typing import _InternalEntityType
+    from ._typing import SynchronizeSessionArgument
     from .mapper import Mapper
     from .path_registry import PathRegistry
-    from .session import Session, _PKIdentityArgument
+    from .session import _PKIdentityArgument
+    from .session import Session
     from .state import InstanceState
+    from ..engine.cursor import CursorResult
+    from ..engine.interfaces import _ImmutableExecuteOptions
+    from ..engine.interfaces import CompiledCacheType
+    from ..engine.interfaces import IsolationLevel
+    from ..engine.interfaces import SchemaTranslateMapType
+    from ..engine.result import FrozenResult
+    from ..engine.result import ScalarResult
+    from ..sql._typing import _ColumnExpressionArgument
+    from ..sql._typing import _ColumnExpressionOrStrLabelArgument
+    from ..sql._typing import _ColumnsClauseArgument
+    from ..sql._typing import _DMLColumnArgument
+    from ..sql._typing import _JoinTargetArgument
+    from ..sql._typing import _LimitOffsetType
+    from ..sql._typing import _MAYBE_ENTITY
+    from ..sql._typing import _no_kw
+    from ..sql._typing import _NOT_ENTITY
+    from ..sql._typing import _OnClauseArgument
+    from ..sql._typing import _PropagateAttrsType
+    from ..sql._typing import _T0
+    from ..sql._typing import _T1
+    from ..sql._typing import _T2
+    from ..sql._typing import _T3
+    from ..sql._typing import _T4
+    from ..sql._typing import _T5
+    from ..sql._typing import _T6
+    from ..sql._typing import _T7
+    from ..sql._typing import _TypedColumnClauseArgument as _TCCA
+    from ..sql.base import CacheableOptions
+    from ..sql.base import ExecutableOption
+    from ..sql.elements import ColumnElement
+    from ..sql.elements import Label
+    from ..sql.selectable import _JoinTargetElement
+    from ..sql.selectable import _SetupJoinsElement
+    from ..sql.selectable import Alias
+    from ..sql.selectable import CTE
+    from ..sql.selectable import ExecutableReturnsRows
+    from ..sql.selectable import FromClause
+    from ..sql.selectable import ScalarSelect
+    from ..sql.selectable import Subquery
+    from .decl_api import DeclarativeBase
 
 
 __all__ = ["Query", "QueryContext"]
@@ -1786,7 +1791,7 @@ class Query(
             Union[
                 _ColumnExpressionArgument[Any],
                 Sequence[_ColumnExpressionArgument[Any]],
-                type[DeclarativeBase],
+                type[DeclarativeBase]
             ]
         ] = None,
         skip_locked: bool = False,
