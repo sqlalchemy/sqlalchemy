1da3f3455dc97ad095d7abd10add7f12efe6c1c7
Federico Caselli <cfederico87@gmail.com>
2023-11-08 21:49:06 +0100

    Fix lint error
    Change-Id: Ifb53e125fc9fd759938908710b2474656dbf1ef9

--- doc/build/conf.py
+++ doc/build/conf.py
@@ -22,7 +22,7 @@ sys.path.insert(0, os.path.abspath("../../lib"))
 sys.path.insert(0, os.path.abspath("../.."))  # examples
 
 # was never needed, does not work as of python 3.12 due to conflicts
-#sys.path.insert(0, os.path.abspath("."))
+# sys.path.insert(0, os.path.abspath("."))
 
 
 os.environ["DISABLE_SQLALCHEMY_CEXT_RUNTIME"] = "true"
