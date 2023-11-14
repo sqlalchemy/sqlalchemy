4006cb38e13ac471655f5f27102678ed8933ee60
Jim Bosch <jbosch@astro.princeton.edu>
2023-11-14 16:19:31 -0500

    Fix typing generics in PostgreSQL range types.
    Correctly type PostgreSQL RANGE and MULTIRANGE types as ``Range[T]``
    and ``Sequence[Range[T]]``.
    Introduced utility sequence ``MultiRange`` to allow better
    interoperability of MULTIRANGE types.
    
    Fixes #9736
    Closes: #10625
    Pull-request: https://github.com/sqlalchemy/sqlalchemy/pull/10625
    Pull-request-sha: 2c17bc5f922a2bdb805a29e458184076ccc08055
    
    Change-Id: I4f91d0233b29fd8101e67bdd4cd0aa2524ab788a

--- lib/sqlalchemy/dialects/postgresql/__init__.py
+++ lib/sqlalchemy/dialects/postgresql/__init__.py
@@ -57,12 +57,14 @@ from .named_types import ENUM
 from .named_types import NamedType
 from .ranges import AbstractMultiRange
 from .ranges import AbstractRange
+from .ranges import AbstractSingleRange
 from .ranges import DATEMULTIRANGE
 from .ranges import DATERANGE
 from .ranges import INT4MULTIRANGE
 from .ranges import INT4RANGE
 from .ranges import INT8MULTIRANGE
 from .ranges import INT8RANGE
+from .ranges import MultiRange
 from .ranges import NUMMULTIRANGE
 from .ranges import NUMRANGE
 from .ranges import Range
