b80e17c3f3e21059ba1a425d75bf3e0f9384d4d2
eXenon <xavier.nunn@protonmail.com>
2023-12-04 12:39:25 -0500

    Replace custom URL-encoding method with quote_plus
    Fixed URL-encoding of the username and password components of
    :class:`.engine.URL` objects when converting them to string using the
    :meth:`_engine.URL.render_as_string` method, by using Python standard
    library ``urllib.parse.quote_plus``, rather than the legacy home-grown
    routine from many years ago. Pull request courtesy of Xavier NUNN.
    
    Fixes: #10662
    Closes: #10726
    Pull-request: https://github.com/sqlalchemy/sqlalchemy/pull/10726
    Pull-request-sha: 82219041b8f73d8c932cc40e87c002b3b853e02e
    
    Change-Id: I90b7a9f4dfdb719082b4b178ad4e009a8531a18e

--- lib/sqlalchemy/engine/url.py
+++ lib/sqlalchemy/engine/url.py
@@ -631,7 +631,7 @@ class URL(NamedTuple):
             s += "@"
         if self.host is not None:
             if ":" in self.host:
-                s += "[%s]" % self.host
+                s += f"[{self.host}]"
             else:
                 s += self.host
         if self.port is not None:
@@ -642,7 +642,7 @@ class URL(NamedTuple):
             keys = list(self.query)
             keys.sort()
             s += "?" + "&".join(
-                "%s=%s" % (quote_plus(k), quote_plus(element))
+                f"{_sqla_url_quote(k)}={_sqla_url_quote(element)}"
                 for k in keys
                 for element in util.to_list(self.query[k])
             )
@@ -906,8 +906,7 @@ def _parse_url(name: str) -> URL:
         )
 
 
-def _sqla_url_quote(text: str) -> str:
-    return re.sub(r"[:@/]", lambda m: "%%%X" % ord(m.group(0)), text)
+_sqla_url_quote = quote_plus
 
 
 _sqla_url_unquote = unquote
