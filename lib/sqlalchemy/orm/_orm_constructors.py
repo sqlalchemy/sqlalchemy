2db07e0c8ffb542df6140ca3e31596d0b33bfb00
opkna <oscar.persson1993@gmail.com>
2024-07-04 22:13:10 +0200

    Added valid types to server_onupdate (#11555)
    * Added valid types to server_onupdate and mapped_column kwargs mypy tests
    * Joined mapped_column test files
    * Set _ServerOnUpdateArgument to _ServerDefaultArgument
    
    Fixes: #11546
    (cherry picked from commit f979aff468a4bdc32aad7b073583823cddf8f21c)

--- lib/sqlalchemy/orm/_orm_constructors.py
+++ lib/sqlalchemy/orm/_orm_constructors.py
@@ -70,7 +70,7 @@ if TYPE_CHECKING:
     from ..sql._typing import _TypeEngineArgument
     from ..sql.elements import ColumnElement
     from ..sql.schema import _ServerDefaultArgument
-    from ..sql.schema import FetchedValue
+    from ..sql.schema import _ServerOnUpdateArgument
     from ..sql.selectable import Alias
     from ..sql.selectable import Subquery
 
@@ -127,7 +127,7 @@ def mapped_column(
     onupdate: Optional[Any] = None,
     insert_default: Optional[Any] = _NoArg.NO_ARG,
     server_default: Optional[_ServerDefaultArgument] = None,
-    server_onupdate: Optional[FetchedValue] = None,
+    server_onupdate: Optional[_ServerOnUpdateArgument] = None,
     active_history: bool = False,
     quote: Optional[bool] = None,
     system: bool = False,
