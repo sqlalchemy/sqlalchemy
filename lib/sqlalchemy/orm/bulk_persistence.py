7001429a7561b3c55dd52b96dfa419004e535743
Mike Bayer <mike_mp@zzzcomputing.com>
2024-08-01 09:49:55 -0400

    mutate lists in place for return_defaults=True
    Fixed regression from version 1.4 in
    :meth:`_orm.Session.bulk_insert_mappings` where using the
    :paramref:`_orm.Session.bulk_insert_mappings.return_defaults` parameter
    would not populate the passed in dictionaries with newly generated primary
    key values.
    
    Fixes: #11661
    Change-Id: I331d81a5b04456f107eb868f882d67773b3eec38

--- lib/sqlalchemy/orm/bulk_persistence.py
+++ lib/sqlalchemy/orm/bulk_persistence.py
@@ -121,13 +121,35 @@ def _bulk_insert(
         )
 
     if isstates:
+        if TYPE_CHECKING:
+            mappings = cast(Iterable[InstanceState[_O]], mappings)
+
         if return_defaults:
+            # list of states allows us to attach .key for return_defaults case
             states = [(state, state.dict) for state in mappings]
             mappings = [dict_ for (state, dict_) in states]
         else:
             mappings = [state.dict for state in mappings]
     else:
-        mappings = [dict(m) for m in mappings]
+        if TYPE_CHECKING:
+            mappings = cast(Iterable[Dict[str, Any]], mappings)
+
+        if return_defaults:
+            # use dictionaries given, so that newly populated defaults
+            # can be delivered back to the caller (see #11661). This is **not**
+            # compatible with other use cases such as a session-executed
+            # insert() construct, as this will confuse the case of
+            # insert-per-subclass for joined inheritance cases (see
+            # test_bulk_statements.py::BulkDMLReturningJoinedInhTest).
+            #
+            # So in this conditional, we have **only** called
+            # session.bulk_insert_mappings() which does not have this
+            # requirement
+            mappings = list(mappings)
+        else:
+            # for all other cases we need to establish a local dictionary
+            # so that the incoming dictionaries aren't mutated
+            mappings = [dict(m) for m in mappings]
         _expand_composites(mapper, mappings)
 
     connection = session_transaction.connection(base_mapper)
