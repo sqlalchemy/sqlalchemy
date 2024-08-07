efa344b54ad2b6c7856dc1e40d01fc7dda2f7974
Jeff Horemans <jeff.horemans@vortex-financials.be>
2024-08-07 13:09:00 +0200

    Handle multi-line check constraint definition extraction on SQLite. Fixes #11677.

--- lib/sqlalchemy/dialects/sqlite/base.py
+++ lib/sqlalchemy/dialects/sqlite/base.py
@@ -2624,7 +2624,7 @@ class SQLiteDialect(default.DefaultDialect):
             connection, table_name, schema=schema, **kw
         )
 
-        CHECK_PATTERN = r"(?:CONSTRAINT (.+) +)?" r"CHECK *\( *(.+) *\),? *"
+        CHECK_PATTERN = r"(?:CONSTRAINT (.+) +)?CHECK *\( *((.|\n)+?) *\)(?:, ?\n|\n) *"
         cks = []
         # NOTE: we aren't using re.S here because we actually are
         # taking advantage of each CHECK constraint being all on one
@@ -2632,7 +2632,7 @@ class SQLiteDialect(default.DefaultDialect):
         # necessarily makes assumptions as to how the CREATE TABLE
         # was emitted.
 
-        for match in re.finditer(CHECK_PATTERN, table_data or "", re.I):
+        for match in re.finditer(CHECK_PATTERN, table_data or "", re.I|re.S):
             name = match.group(1)
 
             if name:
