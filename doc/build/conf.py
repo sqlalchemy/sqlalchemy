ca501d9d38d50dc4eda52c82ebc9c51766bc141b
Mike Bayer <mike_mp@zzzcomputing.com>
2024-09-16 16:01:26 -0400

    - 2.0.35

--- doc/build/conf.py
+++ doc/build/conf.py
@@ -244,9 +244,9 @@ copyright = "2007-2024, the SQLAlchemy authors and contributors"  # noqa
 # The short X.Y version.
 version = "2.0"
 # The full version, including alpha/beta/rc tags.
-release = "2.0.34"
+release = "2.0.35"
 
-release_date = "September 4, 2024"
+release_date = "September 16, 2024"
 
 site_base = os.environ.get("RTD_SITE_BASE", "https://www.sqlalchemy.org")
 site_adapter_template = "docs_adapter.mako"
