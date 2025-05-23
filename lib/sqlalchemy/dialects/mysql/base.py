sqlalchemy/sqlalchemy

commit 51a7678db2f0fcb1552afa40333640bc7fbb6dac

Type mysql dialect
Closes: #12164
Pull-request: https://github.com/sqlalchemy/sqlalchemy/pull/12164
Pull-request-sha: 545e2c39d5ee4f3938111b26e098fa2aa2b6e800
Co-authored-by: Mike Bayer <mike_mp@zzzcomputing.com>
Change-Id: I37bd98049ff1a64d58e9490b0e5e2ea764dd1f73

lib/sqlalchemy/dialects/mysql/mysqlconnector.py
@@ -4,7 +4,6 @@
 #
 # This module is part of SQLAlchemy and is released under
 # the MIT License: https://www.opensource.org/licenses/mit-license.php
-# mypy: ignore-errors
 
 
 r"""
@@ -46,29 +45,54 @@ charset/collation will allow connectivity.
 
 
 """  # noqa
+from __future__ import annotations
 
 import re
+from typing import Any
+from typing import cast
+from typing import Optional
+from typing import Sequence
+from typing import TYPE_CHECKING
+from typing import Union
 
-from .base import BIT
 from .base import MariaDBIdentifierPreparer
 from .base import MySQLCompiler
 from .base import MySQLDialect
 from .base import MySQLExecutionContext
 from .base import MySQLIdentifierPreparer
 from .mariadb import MariaDBDialect
+from .types import BIT
 from ... import util
+
+if TYPE_CHECKING:
+
+    from ...engine.base import Connection
+    from ...engine.cursor import CursorResult
+    from ...engine.interfaces import ConnectArgsType
+    from ...engine.interfaces import DBAPIConnection
+    from ...engine.interfaces import DBAPICursor
+    from ...engine.interfaces import DBAPIModule
+    from ...engine.interfaces import IsolationLevel
+    from ...engine.interfaces import PoolProxiedConnection
+    from ...engine.row import Row
+    from ...engine.url import URL
+    from ...sql.elements import BinaryExpression
+    from ...util.typing import TupleAny
+    from ...util.typing import Unpack
 
 
 class MySQLExecutionContext_mysqlconnector(MySQLExecutionContext):
-    def create_server_side_cursor(self):
+    def create_server_side_cursor(self) -> DBAPICursor:
         return self._dbapi_connection.cursor(buffered=False)
 
-    def create_default_cursor(self):
+    def create_default_cursor(self) -> DBAPICursor:
         return self._dbapi_connection.cursor(buffered=True)
 
 
 class MySQLCompiler_mysqlconnector(MySQLCompiler):
-    def visit_mod_binary(self, binary, operator, **kw):
+    def visit_mod_binary(
+        self, binary: BinaryExpression[Any], operator: Any, **kw: Any
+    ) -> str:
         return (
             self.process(binary.left, **kw)
             + " % "
@@ -78,32 +102,35 @@ class MySQLCompiler_mysqlconnector(MySQLCompiler):
 
 class IdentifierPreparerCommon_mysqlconnector:
     @property
-    def _double_percents(self):
+    def _double_percents(self) -> bool:
         return False
 
     @_double_percents.setter
-    def _double_percents(self, value):
+    def _double_percents(self, value: Any) -> None:
         pass
 
-    def _escape_identifier(self, value):
-        value = value.replace(self.escape_quote, self.escape_to_quote)
+    def _escape_identifier(self, value: str) -> str:
+        value = value.replace(
+            self.escape_quote,  # type:ignore[attr-defined]
+            self.escape_to_quote,  # type:ignore[attr-defined]
+        )
         return value
 
 
-class MySQLIdentifierPreparer_mysqlconnector(
+class MySQLIdentifierPreparer_mysqlconnector(  # type:ignore[misc]
     IdentifierPreparerCommon_mysqlconnector, MySQLIdentifierPreparer
 ):
     pass
 
 
-class MariaDBIdentifierPreparer_mysqlconnector(
+class MariaDBIdentifierPreparer_mysqlconnector(  # type:ignore[misc]
     IdentifierPreparerCommon_mysqlconnector, MariaDBIdentifierPreparer
 ):
     pass
 
 
 class _myconnpyBIT(BIT):
-    def result_processor(self, dialect, coltype):
+    def result_processor(self, dialect: Any, coltype: Any) -> None:
         """MySQL-connector already converts mysql bits, so."""
 
         return None
@@ -128,21 +155,21 @@ class MySQLDialect_mysqlconnector(MySQLDialect):
 
     execution_ctx_cls = MySQLExecutionContext_mysqlconnector
 
-    preparer = MySQLIdentifierPreparer_mysqlconnector
+    preparer: type[MySQLIdentifierPreparer] = (
+        MySQLIdentifierPreparer_mysqlconnector
+    )
 
     colspecs = util.update_copy(MySQLDialect.colspecs, {BIT: _myconnpyBIT})
 
     @classmethod
-    def import_dbapi(cls):
-        from mysql import connector
+    def import_dbapi(cls) -> DBAPIModule:
+        return cast(DBAPIModule, __import__("mysql.connector").connector)
 
-        return connector
-
-    def do_ping(self, dbapi_connection):
+    def do_ping(self, dbapi_connection: DBAPIConnection) -> bool:
         dbapi_connection.ping(False)
         return True
 
-    def create_connect_args(self, url):
+    def create_connect_args(self, url: URL) -> ConnectArgsType:
         opts = url.translate_connect_args(username="user")
 
         opts.update(url.query)
@@ -177,7 +204,9 @@ class MySQLDialect_mysqlconnector(MySQLDialect):
         # supports_sane_rowcount.
         if self.dbapi is not None:
             try:
-                from mysql.connector.constants import ClientFlag
+                from mysql.connector import constants  # type: ignore
+
+                ClientFlag = constants.ClientFlag
 
                 client_flags = opts.get(
                     "client_flags", ClientFlag.get_default()
@@ -187,27 +216,33 @@ class MySQLDialect_mysqlconnector(MySQLDialect):
             except Exception:
                 pass
 
-        return [[], opts]
+        return [], opts
 
     @util.memoized_property
-    def _mysqlconnector_version_info(self):
+    def _mysqlconnector_version_info(self) -> Optional[tuple[int, ...]]:
         if self.dbapi and hasattr(self.dbapi, "__version__"):
             m = re.match(r"(\d+)\.(\d+)(?:\.(\d+))?", self.dbapi.__version__)
             if m:
                 return tuple(int(x) for x in m.group(1, 2, 3) if x is not None)
+        return None
 
-    def _detect_charset(self, connection):
-        return connection.connection.charset
+    def _detect_charset(self, connection: Connection) -> str:
+        return connection.connection.charset  # type: ignore
 
-    def _extract_error_code(self, exception):
-        return exception.errno
+    def _extract_error_code(self, exception: BaseException) -> int:
+        return exception.errno  # type: ignore
 
-    def is_disconnect(self, e, connection, cursor):
+    def is_disconnect(
+        self,
+        e: Exception,
+        connection: Optional[Union[PoolProxiedConnection, DBAPIConnection]],
+        cursor: Optional[DBAPICursor],
+    ) -> bool:
         errnos = (2006, 2013, 2014, 2045, 2055, 2048)
         exceptions = (
-            self.dbapi.OperationalError,
-            self.dbapi.InterfaceError,
-            self.dbapi.ProgrammingError,
+            self.loaded_dbapi.OperationalError,  #
+            self.loaded_dbapi.InterfaceError,
+            self.loaded_dbapi.ProgrammingError,
         )
         if isinstance(e, exceptions):
             return (
@@ -218,13 +253,23 @@ class MySQLDialect_mysqlconnector(MySQLDialect):
         else:
             return False
 
-    def _compat_fetchall(self, rp, charset=None):
+    def _compat_fetchall(
+        self,
+        rp: CursorResult[Unpack[TupleAny]],
+        charset: Optional[str] = None,
+    ) -> Sequence[Row[Unpack[TupleAny]]]:
         return rp.fetchall()
 
-    def _compat_fetchone(self, rp, charset=None):
+    def _compat_fetchone(
+        self,
+        rp: CursorResult[Unpack[TupleAny]],
+        charset: Optional[str] = None,
+    ) -> Optional[Row[Unpack[TupleAny]]]:
         return rp.fetchone()
 
-    def get_isolation_level_values(self, dbapi_connection):
+    def get_isolation_level_values(
+        self, dbapi_conn: DBAPIConnection
+    ) -> Sequence[IsolationLevel]:
         return (
             "SERIALIZABLE",
             "READ UNCOMMITTED",
@@ -233,12 +278,14 @@ class MySQLDialect_mysqlconnector(MySQLDialect):
             "AUTOCOMMIT",
         )
 
-    def set_isolation_level(self, connection, level):
+    def set_isolation_level(
+        self, dbapi_connection: DBAPIConnection, level: IsolationLevel
+    ) -> None:
         if level == "AUTOCOMMIT":
-            connection.autocommit = True
+            dbapi_connection.autocommit = True
         else:
-            connection.autocommit = False
-            super().set_isolation_level(connection, level)
+            dbapi_connection.autocommit = False
+            super().set_isolation_level(dbapi_connection, level)
 
 
 class MariaDBDialect_mysqlconnector(

lib/sqlalchemy/pool/base.py
@@ -1077,6 +1077,8 @@ class PoolProxiedConnection(ManagesConnection):
 
         def rollback(self) -> None: ...
 
+        def __getattr__(self, key: str) -> Any: ...
+
     @property
     def is_valid(self) -> bool:
         """Return True if this :class:`.PoolProxiedConnection` still refers

lib/sqlalchemy/connectors/asyncio.py
@@ -20,13 +20,17 @@ from typing import NoReturn
 from typing import Optional
 from typing import Protocol
 from typing import Sequence
+from typing import TYPE_CHECKING
 
 from ..engine import AdaptedConnection
-from ..engine.interfaces import _DBAPICursorDescription
-from ..engine.interfaces import _DBAPIMultiExecuteParams
-from ..engine.interfaces import _DBAPISingleExecuteParams
 from ..util.concurrency import await_
-from ..util.typing import Self
+
+if TYPE_CHECKING:
+    from ..engine.interfaces import _DBAPICursorDescription
+    from ..engine.interfaces import _DBAPIMultiExecuteParams
+    from ..engine.interfaces import _DBAPISingleExecuteParams
+    from ..engine.interfaces import DBAPIModule
+    from ..util.typing import Self
 
 
 class AsyncIODBAPIConnection(Protocol):
@@ -36,13 +40,18 @@ class AsyncIODBAPIConnection(Protocol):
 
     """
 
-    async def close(self) -> None: ...
+    # note that async DBAPIs dont agree if close() should be awaitable,
+    # so it is omitted here and picked up by the __getattr__ hook below
 
     async def commit(self) -> None: ...
 
     def cursor(self, *args: Any, **kwargs: Any) -> AsyncIODBAPICursor: ...
 
     async def rollback(self) -> None: ...
+
+    def __getattr__(self, key: str) -> Any: ...
+
+    def __setattr__(self, key: str, value: Any) -> None: ...
 
 
 class AsyncIODBAPICursor(Protocol):
@@ -99,6 +108,16 @@ class AsyncIODBAPICursor(Protocol):
     async def nextset(self) -> Optional[bool]: ...
 
     def __aiter__(self) -> AsyncIterator[Any]: ...
+
+
+class AsyncAdapt_dbapi_module:
+    if TYPE_CHECKING:
+        Error = DBAPIModule.Error
+        OperationalError = DBAPIModule.OperationalError
+        InterfaceError = DBAPIModule.InterfaceError
+        IntegrityError = DBAPIModule.IntegrityError
+
+        def __getattr__(self, key: str) -> Any: ...
 
 
 class AsyncAdapt_dbapi_cursor:

lib/sqlalchemy/connectors/pyodbc.py
@@ -8,7 +8,6 @@
 from __future__ import annotations
 
 import re
-from types import ModuleType
 import typing
 from typing import Any
 from typing import Dict
@@ -28,6 +27,7 @@ from ..engine import URL
 from ..sql.type_api import TypeEngine
 
 if typing.TYPE_CHECKING:
+    from ..engine.interfaces import DBAPIModule
     from ..engine.interfaces import IsolationLevel
 
 
@@ -47,15 +47,13 @@ class PyODBCConnector(Connector):
     # hold the desired driver name
     pyodbc_driver_name: Optional[str] = None
 
-    dbapi: ModuleType
-
     def __init__(self, use_setinputsizes: bool = False, **kw: Any):
         super().__init__(**kw)
         if use_setinputsizes:
             self.bind_typing = interfaces.BindTyping.SETINPUTSIZES
 
     @classmethod
-    def import_dbapi(cls) -> ModuleType:
+    def import_dbapi(cls) -> DBAPIModule:
         return __import__("pyodbc")
 
     def create_connect_args(self, url: URL) -> ConnectArgsType:
@@ -150,7 +148,7 @@ class PyODBCConnector(Connector):
         ],
         cursor: Optional[interfaces.DBAPICursor],
     ) -> bool:
-        if isinstance(e, self.dbapi.ProgrammingError):
+        if isinstance(e, self.loaded_dbapi.ProgrammingError):
             return "The cursor's connection has been closed." in str(
                 e
             ) or "Attempt to use a closed connection." in str(e)

lib/sqlalchemy/dialects/__init__.py
@@ -7,6 +7,7 @@
 
 from __future__ import annotations
 
+from typing import Any
 from typing import Callable
 from typing import Optional
 from typing import Type
@@ -39,7 +40,7 @@ def _auto_fn(name: str) -> Optional[Callable[[], Type[Dialect]]]:
             # hardcoded.   if mysql / mariadb etc were third party dialects
             # they would just publish all the entrypoints, which would actually
             # look much nicer.
-            module = __import__(
+            module: Any = __import__(
                 "sqlalchemy.dialects.mysql.mariadb"
             ).dialects.mysql.mariadb
             return module.loader(driver)  # type: ignore

lib/sqlalchemy/dialects/mysql/aiomysql.py
@@ -4,7 +4,6 @@
 #
 # This module is part of SQLAlchemy and is released under
 # the MIT License: https://www.opensource.org/licenses/mit-license.php
-# mypy: ignore-errors
 
 r"""
 .. dialect:: mysql+aiomysql
@@ -29,17 +28,39 @@ This dialect should normally be used only with the
     )
 
 """  # noqa
+from __future__ import annotations
+
+from types import ModuleType
+from typing import Any
+from typing import Optional
+from typing import TYPE_CHECKING
+from typing import Union
+
 from .pymysql import MySQLDialect_pymysql
 from ...connectors.asyncio import AsyncAdapt_dbapi_connection
 from ...connectors.asyncio import AsyncAdapt_dbapi_cursor
+from ...connectors.asyncio import AsyncAdapt_dbapi_module
 from ...connectors.asyncio import AsyncAdapt_dbapi_ss_cursor
 from ...util.concurrency import await_
+
+if TYPE_CHECKING:
+
+    from ...connectors.asyncio import AsyncIODBAPIConnection
+    from ...connectors.asyncio import AsyncIODBAPICursor
+    from ...engine.interfaces import ConnectArgsType
+    from ...engine.interfaces import DBAPIConnection
+    from ...engine.interfaces import DBAPICursor
+    from ...engine.interfaces import DBAPIModule
+    from ...engine.interfaces import PoolProxiedConnection
+    from ...engine.url import URL
 
 
 class AsyncAdapt_aiomysql_cursor(AsyncAdapt_dbapi_cursor):
     __slots__ = ()
 
-    def _make_new_cursor(self, connection):
+    def _make_new_cursor(
+        self, connection: AsyncIODBAPIConnection
+    ) -> AsyncIODBAPICursor:
         return connection.cursor(self._adapt_connection.dbapi.Cursor)
 
 
@@ -48,7 +69,9 @@ class AsyncAdapt_aiomysql_ss_cursor(
 ):
     __slots__ = ()
 
-    def _make_new_cursor(self, connection):
+    def _make_new_cursor(
+        self, connection: AsyncIODBAPIConnection
+    ) -> AsyncIODBAPICursor:
         return connection.cursor(
             self._adapt_connection.dbapi.aiomysql.cursors.SSCursor
         )
@@ -60,17 +83,17 @@ class AsyncAdapt_aiomysql_connection(AsyncAdapt_dbapi_connection):
     _cursor_cls = AsyncAdapt_aiomysql_cursor
     _ss_cursor_cls = AsyncAdapt_aiomysql_ss_cursor
 
-    def ping(self, reconnect):
+    def ping(self, reconnect: bool) -> None:
         assert not reconnect
-        return await_(self._connection.ping(reconnect))
+        await_(self._connection.ping(reconnect))
 
-    def character_set_name(self):
-        return self._connection.character_set_name()
+    def character_set_name(self) -> Optional[str]:
+        return self._connection.character_set_name()  # type: ignore[no-any-return]  # noqa: E501
 
-    def autocommit(self, value):
+    def autocommit(self, value: Any) -> None:
         await_(self._connection.autocommit(value))
 
-    def terminate(self):
+    def terminate(self) -> None:
         # it's not awaitable.
         self._connection.close()
 
@@ -78,15 +101,15 @@ class AsyncAdapt_aiomysql_connection(AsyncAdapt_dbapi_connection):
         await_(self._connection.ensure_closed())
 
 
-class AsyncAdapt_aiomysql_dbapi:
-    def __init__(self, aiomysql, pymysql):
+class AsyncAdapt_aiomysql_dbapi(AsyncAdapt_dbapi_module):
+    def __init__(self, aiomysql: ModuleType, pymysql: ModuleType):
         self.aiomysql = aiomysql
         self.pymysql = pymysql
         self.paramstyle = "format"
         self._init_dbapi_attributes()
         self.Cursor, self.SSCursor = self._init_cursors_subclasses()
 
-    def _init_dbapi_attributes(self):
+    def _init_dbapi_attributes(self) -> None:
         for name in (
             "Warning",
             "Error",
@@ -112,7 +135,7 @@ class AsyncAdapt_aiomysql_dbapi:
         ):
             setattr(self, name, getattr(self.pymysql, name))
 
-    def connect(self, *arg, **kw):
+    def connect(self, *arg: Any, **kw: Any) -> AsyncAdapt_aiomysql_connection:
         creator_fn = kw.pop("async_creator_fn", self.aiomysql.connect)
 
         return AsyncAdapt_aiomysql_connection(
@@ -120,57 +143,72 @@ class AsyncAdapt_aiomysql_dbapi:
             await_(creator_fn(*arg, **kw)),
         )
 
-    def _init_cursors_subclasses(self):
+    def _init_cursors_subclasses(
+        self,
+    ) -> tuple[AsyncIODBAPICursor, AsyncIODBAPICursor]:
         # suppress unconditional warning emitted by aiomysql
-        class Cursor(self.aiomysql.Cursor):
-            async def _show_warnings(self, conn):
+        class Cursor(self.aiomysql.Cursor):  # type: ignore[misc, name-defined]
+            async def _show_warnings(
+                self, conn: AsyncIODBAPIConnection
+            ) -> None:
                 pass
 
-        class SSCursor(self.aiomysql.SSCursor):
-            async def _show_warnings(self, conn):
+        class SSCursor(self.aiomysql.SSCursor):  # type: ignore[misc, name-defined]   # noqa: E501
+            async def _show_warnings(
+                self, conn: AsyncIODBAPIConnection
+            ) -> None:
                 pass
 
-        return Cursor, SSCursor
+        return Cursor, SSCursor  # type: ignore[return-value]
 
 
 class MySQLDialect_aiomysql(MySQLDialect_pymysql):
     driver = "aiomysql"
     supports_statement_cache = True
 
-    supports_server_side_cursors = True
+    supports_server_side_cursors = True  # type: ignore[assignment]
     _sscursor = AsyncAdapt_aiomysql_ss_cursor
 
     is_async = True
     has_terminate = True
 
     @classmethod
-    def import_dbapi(cls):
+    def import_dbapi(cls) -> AsyncAdapt_aiomysql_dbapi:
         return AsyncAdapt_aiomysql_dbapi(
             __import__("aiomysql"), __import__("pymysql")
         )
 
-    def do_terminate(self, dbapi_connection) -> None:
+    def do_terminate(self, dbapi_connection: DBAPIConnection) -> None:
         dbapi_connection.terminate()
 
-    def create_connect_args(self, url):
+    def create_connect_args(
+        self, url: URL, _translate_args: Optional[dict[str, Any]] = None
+    ) -> ConnectArgsType:
         return super().create_connect_args(
             url, _translate_args=dict(username="user", database="db")
         )
 
-    def is_disconnect(self, e, connection, cursor):
+    def is_disconnect(
+        self,
+        e: DBAPIModule.Error,
+        connection: Optional[Union[PoolProxiedConnection, DBAPIConnection]],
+        cursor: Optional[DBAPICursor],
+    ) -> bool:
         if super().is_disconnect(e, connection, cursor):
             return True
         else:
             str_e = str(e).lower()
             return "not connected" in str_e
 
-    def _found_rows_client_flag(self):
-        from pymysql.constants import CLIENT
+    def _found_rows_client_flag(self) -> int:
+        from pymysql.constants import CLIENT  # type: ignore
 
-        return CLIENT.FOUND_ROWS
+        return CLIENT.FOUND_ROWS  # type: ignore[no-any-return]
 
-    def get_driver_connection(self, connection):
-        return connection._connection
+    def get_driver_connection(
+        self, connection: DBAPIConnection
+    ) -> AsyncIODBAPIConnection:
+        return connection._connection  # type: ignore[no-any-return]
 
 
 dialect = MySQLDialect_aiomysql

lib/sqlalchemy/dialects/mysql/asyncmy.py
@@ -4,7 +4,6 @@
 #
 # This module is part of SQLAlchemy and is released under
 # the MIT License: https://www.opensource.org/licenses/mit-license.php
-# mypy: ignore-errors
 
 r"""
 .. dialect:: mysql+asyncmy
@@ -29,12 +28,31 @@ This dialect should normally be used only with the
 """  # noqa
 from __future__ import annotations
 
+from types import ModuleType
+from typing import Any
+from typing import NoReturn
+from typing import Optional
+from typing import TYPE_CHECKING
+from typing import Union
+
 from .pymysql import MySQLDialect_pymysql
 from ... import util
 from ...connectors.asyncio import AsyncAdapt_dbapi_connection
 from ...connectors.asyncio import AsyncAdapt_dbapi_cursor
+from ...connectors.asyncio import AsyncAdapt_dbapi_module
 from ...connectors.asyncio import AsyncAdapt_dbapi_ss_cursor
 from ...util.concurrency import await_
+
+if TYPE_CHECKING:
+
+    from ...connectors.asyncio import AsyncIODBAPIConnection
+    from ...connectors.asyncio import AsyncIODBAPICursor
+    from ...engine.interfaces import ConnectArgsType
+    from ...engine.interfaces import DBAPIConnection
+    from ...engine.interfaces import DBAPICursor
+    from ...engine.interfaces import DBAPIModule
+    from ...engine.interfaces import PoolProxiedConnection
+    from ...engine.url import URL
 
 
 class AsyncAdapt_asyncmy_cursor(AsyncAdapt_dbapi_cursor):
@@ -46,7 +64,9 @@ class AsyncAdapt_asyncmy_ss_cursor(
 ):
     __slots__ = ()
 
-    def _make_new_cursor(self, connection):
+    def _make_new_cursor(
+        self, connection: AsyncIODBAPIConnection
+    ) -> AsyncIODBAPICursor:
         return connection.cursor(
             self._adapt_connection.dbapi.asyncmy.cursors.SSCursor
         )
@@ -58,7 +78,7 @@ class AsyncAdapt_asyncmy_connection(AsyncAdapt_dbapi_connection):
     _cursor_cls = AsyncAdapt_asyncmy_cursor
     _ss_cursor_cls = AsyncAdapt_asyncmy_ss_cursor
 
-    def _handle_exception(self, error):
+    def _handle_exception(self, error: Exception) -> NoReturn:
         if isinstance(error, AttributeError):
             raise self.dbapi.InternalError(
                 "network operation failed due to asyncmy attribute error"
@@ -66,24 +86,24 @@ class AsyncAdapt_asyncmy_connection(AsyncAdapt_dbapi_connection):
 
         raise error
 
-    def ping(self, reconnect):
+    def ping(self, reconnect: bool) -> None:
         assert not reconnect
         return await_(self._do_ping())
 
-    async def _do_ping(self):
+    async def _do_ping(self) -> None:
         try:
             async with self._execute_mutex:
-                return await self._connection.ping(False)
+                await self._connection.ping(False)
         except Exception as error:
             self._handle_exception(error)
 
-    def character_set_name(self):
-        return self._connection.character_set_name()
+    def character_set_name(self) -> Optional[str]:
+        return self._connection.character_set_name()  # type: ignore[no-any-return]  # noqa: E501
 
-    def autocommit(self, value):
+    def autocommit(self, value: Any) -> None:
         await_(self._connection.autocommit(value))
 
-    def terminate(self):
+    def terminate(self) -> None:
         # it's not awaitable.
         self._connection.close()
 
@@ -91,18 +111,13 @@ class AsyncAdapt_asyncmy_connection(AsyncAdapt_dbapi_connection):
         await_(self._connection.ensure_closed())
 
 
-def _Binary(x):
-    """Return x as a binary type."""
-    return bytes(x)
-
-
-class AsyncAdapt_asyncmy_dbapi:
-    def __init__(self, asyncmy):
+class AsyncAdapt_asyncmy_dbapi(AsyncAdapt_dbapi_module):
+    def __init__(self, asyncmy: ModuleType):
         self.asyncmy = asyncmy
         self.paramstyle = "format"
         self._init_dbapi_attributes()
 
-    def _init_dbapi_attributes(self):
+    def _init_dbapi_attributes(self) -> None:
         for name in (
             "Warning",
             "Error",
@@ -123,9 +138,9 @@ class AsyncAdapt_asyncmy_dbapi:
     BINARY = util.symbol("BINARY")
     DATETIME = util.symbol("DATETIME")
     TIMESTAMP = util.symbol("TIMESTAMP")
-    Binary = staticmethod(_Binary)
+    Binary = staticmethod(bytes)
 
-    def connect(self, *arg, **kw):
+    def connect(self, *arg: Any, **kw: Any) -> AsyncAdapt_asyncmy_connection:
         creator_fn = kw.pop("async_creator_fn", self.asyncmy.connect)
 
         return AsyncAdapt_asyncmy_connection(
@@ -138,25 +153,30 @@ class MySQLDialect_asyncmy(MySQLDialect_pymysql):
     driver = "asyncmy"
     supports_statement_cache = True
 
-    supports_server_side_cursors = True
+    supports_server_side_cursors = True  # type: ignore[assignment]
     _sscursor = AsyncAdapt_asyncmy_ss_cursor
 
     is_async = True
     has_terminate = True
 
     @classmethod
-    def import_dbapi(cls):
+    def import_dbapi(cls) -> DBAPIModule:
         return AsyncAdapt_asyncmy_dbapi(__import__("asyncmy"))
 
-    def do_terminate(self, dbapi_connection) -> None:
+    def do_terminate(self, dbapi_connection: DBAPIConnection) -> None:
         dbapi_connection.terminate()
 
-    def create_connect_args(self, url):
+    def create_connect_args(self, url: URL) -> ConnectArgsType:  # type: ignore[override]  # noqa: E501
         return super().create_connect_args(
             url, _translate_args=dict(username="user", database="db")
         )
 
-    def is_disconnect(self, e, connection, cursor):
+    def is_disconnect(
+        self,
+        e: DBAPIModule.Error,
+        connection: Optional[Union[PoolProxiedConnection, DBAPIConnection]],
+        cursor: Optional[DBAPICursor],
+    ) -> bool:
         if super().is_disconnect(e, connection, cursor):
             return True
         else:
@@ -165,13 +185,15 @@ class MySQLDialect_asyncmy(MySQLDialect_pymysql):
                 "not connected" in str_e or "network operation failed" in str_e
             )
 
-    def _found_rows_client_flag(self):
-        from asyncmy.constants import CLIENT
+    def _found_rows_client_flag(self) -> int:
+        from asyncmy.constants import CLIENT  # type: ignore
 
-        return CLIENT.FOUND_ROWS
+        return CLIENT.FOUND_ROWS  # type: ignore[no-any-return]
 
-    def get_driver_connection(self, connection):
-        return connection._connection
+    def get_driver_connection(
+        self, connection: DBAPIConnection
+    ) -> AsyncIODBAPIConnection:
+        return connection._connection  # type: ignore[no-any-return]
 
 
 dialect = MySQLDialect_asyncmy

commit 1070889f263be89e0e47bdbb9f7113e98ead192b

fix missing quotes from cast call in mysqlconnector module
This fixes an issue introduced by 51a7678db2f0fcb1552afa40333640bc7fbb6dac
in I37bd98049ff1a64d58e9490b0e5e2ea764dd1f73

Change-Id: Id738c04ee4dc8c2b12d9ab0fc71a4e1a6c5bc209

lib/sqlalchemy/dialects/mysql/base.py
@@ -3706,7 +3706,7 @@ class MySQLDialect(default.DefaultDialect):
         if not row:
             return None
         else:
-            return cast("Optional[str]", row[fetch_col])
+            return cast(Optional[str], row[fetch_col])
 
     def _detect_charset(self, connection: Connection) -> str:
         raise NotImplementedError()
@@ -3819,7 +3819,7 @@ class MySQLDialect(default.DefaultDialect):
         row = self._compat_first(rp, charset=charset)
         if not row:
             raise exc.NoSuchTableError(full_name)
-        return cast("str", row[1]).strip()
+        return cast(str, row[1]).strip()
 
     @overload
     def _describe_table(

lib/sqlalchemy/dialects/mysql/mysqlconnector.py
@@ -163,7 +163,7 @@ class MySQLDialect_mysqlconnector(MySQLDialect):
 
     @classmethod
     def import_dbapi(cls) -> DBAPIModule:
-        return cast(DBAPIModule, __import__("mysql.connector").connector)
+        return cast("DBAPIModule", __import__("mysql.connector").connector)
 
     def do_ping(self, dbapi_connection: DBAPIConnection) -> bool:
         dbapi_connection.ping(False)