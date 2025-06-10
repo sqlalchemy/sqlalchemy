3a5f4a59495e60d7f979b80c7b33a06ad3de358f
Mike Bayer <mike_mp@zzzcomputing.com>
2025-06-10 17:33:14 -0400

    use integer division on maxlen
    this was coming out as a float and breaking alembic column
    compare
    
    Change-Id: I50160cfdb2f2933331d3c316c9985f24fb914242
    (cherry picked from commit 61477cf8b8af2b5a7123764a564da056f1a5c999)

--- lib/sqlalchemy/dialects/mssql/base.py
+++ lib/sqlalchemy/dialects/mssql/base.py
@@ -3776,7 +3776,7 @@ where
                 MSNChar,
                 MSNText,
             ):
-                kwargs["length"] = maxlen / 2 if maxlen != -1 else None
+                kwargs["length"] = maxlen // 2 if maxlen != -1 else None
                 if collation:
                     kwargs["collation"] = collation
 
--- lib/sqlalchemy/testing/suite/test_reflection.py
+++ lib/sqlalchemy/testing/suite/test_reflection.py
@@ -2870,6 +2870,7 @@ class ComponentReflectionTestExtra(ComparesIndexes, fixtures.TestBase):
             assert isinstance(typ, sql_types.String)
 
         eq_(typ.length, 52)
+        assert isinstance(typ.length, int)
 
     @testing.requires.table_reflection
     def test_nullable_reflection(self, connection, metadata):
