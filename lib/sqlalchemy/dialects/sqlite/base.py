908e29671fd8977123b708db2b5dfd91b3a41355
Mike Bayer <mike_mp@zzzcomputing.com>
2025-08-07 10:48:51 -0400

    ensure autocommit is not False for SQLite FK pragma
    References: #12767
    References: https://github.com/python/cpython/issues/137205
    Change-Id: I6772a4c9c216c6981421043f850cbf833ded2be3
    (cherry picked from commit e96bf8608ed2862f538ac0873303c91ed3a7260c)

--- lib/sqlalchemy/dialects/sqlite/base.py
+++ lib/sqlalchemy/dialects/sqlite/base.py
@@ -396,9 +396,17 @@ new connections through the usage of events::
 
     @event.listens_for(Engine, "connect")
     def set_sqlite_pragma(dbapi_connection, connection_record):
+        # the sqlite3 driver will not set PRAGMA foreign_keys
+        # if autocommit=False; set to True temporarily
+        ac = dbapi_connection.autocommit
+        dbapi_connection.autocommit = True
+
         cursor = dbapi_connection.cursor()
         cursor.execute("PRAGMA foreign_keys=ON")
         cursor.close()
+
+        # restore previous autocommit setting
+        dbapi_connection.autocommit = ac
 
 .. warning::
 
