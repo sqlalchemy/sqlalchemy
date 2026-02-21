0f74af5c339d7f3a6412537571aa60c0180be426
Kadir Can Ozden <101993364+bysiber@users.noreply.github.com>
2026-02-21 06:35:38 -0500

    Fix WeakSequence.__getitem__ catching KeyError instead of IndexError
    ### Description
    
    `WeakSequence.__getitem__` catches `KeyError` but the internal `_storage` is a `list`, which raises `IndexError` for out-of-range access. This means the `except KeyError` handler never executes, and the custom error message is never shown.
    
    ### Current behavior
    
    ```python
    def __getitem__(self, index):
        try:
            obj = self._storage[index]  # _storage is a list
        except KeyError:  # lists don't raise KeyError
            raise IndexError("Index %s out of range" % index)
        else:
            return obj()
    ```
    
    On an out-of-range index, the raw `IndexError` from list access propagates directly (e.g., `list index out of range`) instead of the intended custom message.
    
    ### Fix
    
    Changed `except KeyError` to `except IndexError` so the handler actually catches the exception raised by list indexing.
    
    Closes: #13136
    Pull-request: https://github.com/sqlalchemy/sqlalchemy/pull/13136
    Pull-request-sha: ceb8f9fe46590ec69686769f6736686789c88986
    
    Change-Id: Ia36c2b9b0f3aec97624bcd2a9e49f43b9ed786d9

--- lib/sqlalchemy/util/_collections.py
+++ lib/sqlalchemy/util/_collections.py
@@ -295,12 +295,7 @@ class WeakSequence(Sequence[_T]):
         )
 
     def __getitem__(self, index):
-        try:
-            obj = self._storage[index]
-        except KeyError:
-            raise IndexError("Index %s out of range" % index)
-        else:
-            return obj()
+        return self._storage[index]()
 
 
 OrderedIdentitySet = IdentitySet
