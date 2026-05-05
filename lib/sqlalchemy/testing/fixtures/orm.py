f5981d7e8da4ceaff5b9728d1d4d852b3b05efcd
mattip <matti.picus@gmail.com>
2026-05-05 09:18:36 +0300

    add gc_collect (no-op on CPython) to test teardown

--- lib/sqlalchemy/testing/fixtures/orm.py
+++ lib/sqlalchemy/testing/fixtures/orm.py
@@ -18,6 +18,7 @@ from .. import schema
 from ..entities import BasicEntity
 from ..entities import ComparableEntity
 from ..util import adict
+from ..util import gc_collect
 from ... import orm
 from ...orm import DeclarativeBase
 from ...orm import events as orm_events
@@ -225,3 +226,4 @@ def stop_test_class_inside_fixtures(cls):
 def after_test():
     if _fixture_sessions:
         close_all_sessions()
+    gc_collect()
