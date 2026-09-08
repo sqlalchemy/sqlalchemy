96f67ba284d6545bba8b9a38db50de84b8a7ff71
Michael Bayer <mike_mp@zzzcomputing.com>
2026-09-08 16:39:55 +0000

    Revert "Use weakref.finalize() for connection fairy GC cleanup"
    This reverts commit 79037d05d74208ee3067790e43c61e9e5addb8b5.
    
    Reason for revert: we're having fails on github actions with C extensions built that are very troubling: TypeError: _finalize_fairy() got multiple values for argument 'transaction_was_reset'
    
    I have no idea why that's not happening in our own CI which also seems fairly alarming
    
    Change-Id: I769054742716ce11bba9b450bb52c72af647d0a8

--- doc/build/changelog/unreleased_21/pool_finalize.rst
+++
@@ -1,7 +0,0 @@
-.. change::
-    :tags: changed, engine
-
-    Improved the implementation of :class:`_pool.Pool` to use
-    ``weakref.finalize()`` instead of a ``weakref.ref()`` finalizer to handle
-    GC cleanup of non-detached, pooled connections that were not explicitly
-    closed.
--- test/engine/test_pool.py
+++ test/engine/test_pool.py
@@ -1579,14 +1579,14 @@ class QueuePoolTest(PoolTestBase):
         finalize_fairy = pool._finalize_fairy
 
         def assert_no_wr_callback(
-            connection, connection_record, pool, echo, fairy=None, **kw
+            connection, connection_record, pool, ref, echo, fairy=None
         ):
             if fairy is None:
                 raise AssertionError(
                     "finalize fairy was called as a weakref callback"
                 )
             return finalize_fairy(
-                connection, connection_record, pool, echo, fairy=fairy, **kw
+                connection, connection_record, pool, ref, echo, fairy
             )
 
         return patch.object(pool, "_finalize_fairy", assert_no_wr_callback)
